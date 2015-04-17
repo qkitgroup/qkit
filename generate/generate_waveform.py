e76882db3d9768983b4815295f90ef319b882842
Schneider1 <@>
2015-04-17 07:54:38 +0000

    set_awg was split up to generate_waveform.py and load_awg.py

---
+++ generate/generate_waveform.py
@@ -0,0 +1,204 @@
+import qt
+import numpy as np
+import os.path
+import time
+#import qubit #remove it if not further needed (AS 2015-04-16)
+import logging
+import numpy
+import sys
+
+'''
+	TODO:
+	- low and high are not well defined, as various pulses are summmed up
+	
+	
+'''
+
+# creates dummy objects to have everything well-defined.
+
+global sample, iq
+sample = type('Sample', (object,),{  'exc_T' : 1e-6 , 'tpi' : 2e-9 , 'tpi2' : 1e-9   })
+iq = type('iq', (object,),{'clock':1e9})
+
+
+# awg-supported multiple measurement system
+def erf(pulse, attack, decay, length=None, position = None, clock = None):
+	'''
+		create an erf-shaped envelope function
+		erf(\pm 2) is almost 0/1, erf(\pm 1) is ~15/85%
+		
+		Input:
+			tstart, tstop - erf(-2) times
+			attack, decay - attack/decay times
+	'''
+	import scipy.special
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	sample_start = int(clock*(position-pulse))
+	sample_end = int(clock*position)
+	sample_length = int(np.ceil(length*clock))
+	wfm = np.ones(sample_length)
+	nAttack = int(clock*attack)
+	sAttack = 0.5*(1+scipy.special.erf(np.linspace(-2, 2, nAttack)))
+	wfm[sample_start:sample_start+nAttack] *= sAttack
+	wfm[0:sample_start] *= 0
+	nDecay = int(clock*decay)
+	sDecay = 0.5*(1+scipy.special.erf(np.linspace(2, -2, nDecay)))
+	wfm[(sample_end-nDecay):sample_end] *= sDecay 
+	wfm[sample_end:sample_length] *= 0
+	return wfm
+	
+def triangle(pulse, attack, decay, length = None,position = None, clock = None):
+	'''
+		create a pulse with triangular shape
+		
+		Input:
+			pulse, attack, decay - pulse duration, attack and decay times
+			length - length of the resulting waveform in seconds
+			position - position of the end of the pulse
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	sample_start = int(clock*(position-pulse))
+	sample_end = int(clock*position)
+	sample_length = int(np.ceil(length*clock))
+	sample_attack = int(np.ceil(attack*clock)) 
+	sample_decay = int(np.ceil(decay*clock))
+	wfm = np.zeros(sample_length)
+	wfm[sample_start:sample_start+sample_attack] = np.linspace(0, 1, sample_attack)
+	wfm[sample_start+sample_attack:sample_end-sample_decay] = 1
+	wfm[sample_end-sample_decay:sample_end] = np.linspace(1, 0, sample_decay)
+	return wfm
+
+def square(pulse, length = None,position = None, low = 0, high = 1, clock = None, adddelay=0.,freq=None):
+	'''
+		generate waveform corresponding to a dc pulse
+
+		Input:
+			pulse - pulse duration in seconds
+			length - length of the generated waveform
+			position - time instant of the end of the pulse
+			low - pulse 'off' sample value
+			high - pulse 'on' sample value
+			clock - sample rate of the DAC
+		Output:
+			float array of samples
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	if(pulse>position): logging.error(__name__ + ' : pulse does not fit into waveform')
+	sample_start = int(clock*(position-pulse-adddelay))
+	sample_end = int(clock*(position-adddelay))
+	sample_length = int(np.ceil(length*clock))
+	wfm = low*np.ones(sample_length)
+	if(sample_start < sample_end): wfm[int(sample_start)] = high + (low-high)*(sample_start-int(sample_start))
+	if freq==None: wfm[int(np.ceil(sample_start)):int(sample_end)] = high
+	else:
+		for i in range(int(sample_end)-int(np.ceil(sample_start))):
+			wfm[i+int(np.ceil(sample_start))] = high*np.sin(2*np.pi*freq/clock*(i))
+	if(np.ceil(sample_end) != np.floor(sample_end)): wfm[int(sample_end)] = low + (high-low)*(sample_end-int(sample_end))
+	return wfm
+	
+	
+def gauss(pulse, length = None,position = None, low = 0, high = 1, clock = None):
+	'''
+		generate waveform corresponding to a dc gauss pulse
+
+		Input:
+			pulse - pulse duration in seconds
+			length - length of the generated waveform
+			position - time instant of the end of the pulse
+			low - pulse 'off' sample value
+			high - pulse 'on' sample value
+			clock - sample rate of the DAC
+		Output:
+			float array of samples
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	if(pulse>position): logging.error(__name__ + ' : pulse does not fit into waveform')
+	sample_start = clock*(position-pulse)
+	sample_end = clock*position
+	sample_length = int(np.ceil(length*clock))
+	wfm = low*np.ones(sample_length)
+	if(sample_start < sample_end): wfm[int(sample_start)] = 0.#high + (low-high)*(sample_start-int(sample_start))
+	#wfm[int(np.ceil(sample_start)):int(sample_end)] = high
+	pulsesamples = int(int(sample_end)-np.ceil(sample_start))
+	for i in range(pulsesamples):
+		wfm[np.ceil(sample_start)+i] = high*np.exp(-(i-pulsesamples/2.)**2/(2.*(pulsesamples/5.)**2))
+	if(np.ceil(sample_end) != np.floor(sample_end)): wfm[int(sample_end)] = low + (high-low)*(sample_end-int(sample_end))
+	return wfm
+
+def arb_function(function, pulse, length = None,position = None, clock = None):
+	'''
+		generate arbitrary waveform pulse
+
+		Input:
+			function - function called
+			pulse - duration of the signal
+			position - time instant of the end of the signal
+			length - duration of the waveform
+			clock - sample rate of the DAC
+		Output:
+			float array of samples
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	if(pulse>position): logging.error(__name__ + ' : pulse does not fit into waveform')
+	sample_start = clock*(position-pulse)
+	sample_end = clock*position
+	sample_length = int(np.ceil(length*clock))
+
+	wfm = np.zeros(sample_length)
+	times = 1./clock*np.arange(0, sample_end-sample_start+1)
+	wfm[sample_start:sample_end] = function(times)
+	return wfm
+
+def ramsey(delay, pi2_pulse = None, length = None,position = None, low = 0, high = 1, clock = None):
+	'''
+		generate waveform with two pi/4 pulses and delay in-between
+
+		Input:
+			delay - time delay between the pi/2 pulses
+			pi2_pulse - length of a pi/2 pulse
+			(see awg_pulse for rest)
+		Output:
+			float array of samples
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	if(pi2_pulse == None): pi2_pulse = sample.tpi2
+	if(delay+2*pi2_pulse>position): logging.error(__name__ + ' : ramsey pulses do not fit into waveform')
+	wfm = square(pi2_pulse, length, position, clock = clock)
+	wfm += square(pi2_pulse, length, position-delay-pi2_pulse, clock = clock)
+	wfm = wfm * (high-low) + low
+	return wfm
+
+
+def spinecho(delay, pi2_pulse = None, pi_pulse = None, length = None,position = None, low = 0, high = 1, clock = None, readoutpulse=True,adddelay=0., freq=None):
+	'''
+		generate waveform with two pi/2 pulses, a pi pulse and delays
+		pi2 - delay - pi - delay - [pi2, if readoutpulse]
+		
+		(see awg_ramsey for parameter description)
+	'''
+	if(clock == None): clock= iq.clock
+	if(length == None): length= sample.exc_T
+	if(position == None): position = length
+	if(pi2_pulse == None): pi2_pulse = sample.tpi2
+	if(pi_pulse == None): pi_pulse = sample.tpi
+	if(adddelay+2*delay+2*pi2_pulse+pi_pulse>position): logging.error(__name__ + ' : spin-echo pulses do not fit into waveform')
+	if readoutpulse: wfm = square(pi2_pulse, length, position, clock = clock,freq=freq)
+	else: wfm = square(pi2_pulse, length, position, 0,0, clock,freq=freq)
+	wfm += square(pi_pulse, length, position-pi2_pulse-delay-adddelay, clock = clock,freq=freq)
+	wfm += square(pi2_pulse, length, position-2*delay-1*pi2_pulse-pi_pulse-adddelay, clock = clock,freq=freq)
+	#wfm += square(pi_pulse, length, position-pi_pulse-pi2_pulse-delay-adddelay, low, high, clock,freq=freq)
+	#wfm += square(pi2_pulse, length, position-2*delay-2*pi2_pulse-pi_pulse-adddelay, low, high, clock,freq=freq)
+	wfm = wfm * (high-low) + low
+	return wfm
---
+++ generate/load_awg.py
@@ -0,0 +1,241 @@
+import qt
+import numpy as np
+import os.path
+import time
+import logging
+import numpy
+import sys
+import Progress_Bar
+
+
+
+def update(t, wfm_funcs, wfm_channels, sample, drive = 'c:', path = '\\waveforms'):
+	'''
+		simultaneously perform different experiments on multiple qubits using an awg
+
+		Input:
+			t - parameter for wfm_funcs
+			wfm_funcs - callable function of 1 parameter that generate waveforms
+			wfm_channels - awg channels the waveforms are assigned to
+			sample - the Sample instance, where awg and clock are defined
+	'''
+	awg = sample.get_awg()
+	clock = sample.get_clock()
+	for i in range(len(wfm_funcs)):
+		wfm_func = wfm_funcs[i]
+		wfm_channel = wfm_channels[i]
+		wfm_samples = wfm_func(t)
+		wfm_fn = 'ch%d'%wfm_channel
+		marker = np.zeros(wfm_samples.shape, np.int)
+		awg.send_waveform(wfm_samples, marker, marker, '%s%s\\%s'%(drive, path, wfm_fn), clock)
+		awg.load_waveform(wfm_channel, wfm_fn, drive, path)
+	#awg.run() # necessary?
+
+
+
+def update_sequence(ts, wfm_funcs, wfm_channels, sample, loop = False, drive = 'c:', path = '\\waveforms', reset = True, marker=None):
+	'''
+		set awg to sequence mode and push a number of waveforms into the sequencer
+		
+		for the 6GS/s AWG, the waveform length must be divisible by 64
+		for the 1.2GS/s AWG, it must be divisible by 4
+	'''
+	qt.mstart()
+	awg = sample.get_awg()
+	clock = sample.get_clock()
+	
+	#awg_say = awg._ins._visainstrument.write
+	#awg_ask = awg._ins._visainstrument.ask
+	p = qkit.Progress_Bar.Progress_Bar(len(wfm_funcs)*len(ts))
+	# create new sequence
+	if(reset):
+		awg.set_runmode('SEQ')
+		awg.set_seq_length(0) #awg_say('SEQ:LENG %d'%(0)) # clear sequence
+		awg.set_seq_length(len(ts)) #awg_say('SEQ:LENG %d'%(len(ts))) # create empty sequence
+
+	# update all channels and times
+	for i in range(len(wfm_funcs)):
+		wfm_func = wfm_funcs[i]
+		wfm_channel = wfm_channels[i]
+		wfm_samples_prev = None
+		for ti in range(len(ts)):
+			qt.msleep()
+			t = ts[ti]
+			# filter duplicates
+			wfm_samples = wfm_func(t)
+			if(np.array_equal(wfm_samples, wfm_samples_prev)):
+				# waveform was seen before: just set it in the sequencer
+				pass
+			else:
+				# waveform is new: upload to awg
+				wfm_samples_prev = wfm_samples
+				wfm_fn = 'ch%d_t%05d'%(wfm_channel, ti) # filename is kept until changed
+				wfm_pn = '%s%s\\%s'%(drive, path, wfm_fn)
+				# this results in "low" when the awg is stopped and "high" when it is running 
+				
+				if marker==None:
+					marker1 = np.array([int(0) for j in range(len(wfm_samples))])
+					marker2 = np.array([int(0) for j in range(len(wfm_samples))])
+					#make the markers at least 3 ns
+					#marker samples
+					m_sam = int(clock*3e-9)
+					for j in range(m_sam):
+						marker1[1+j] = int(1)
+						marker2[len(wfm_samples)-1-j] = int(1)
+				else: #or set your own markers
+					if marker == "none":
+						marker1 = np.array([int(0) for j in range(len(wfm_samples))])
+						marker2 = np.array([int(0) for j in range(len(wfm_samples))])
+					else:
+						marker1,marker2 = marker[i]
+				
+				#this was the old marker def
+##				marker = np.ones(wfm_samples.shape, np.int)
+##				marker[-1] = 0
+				awg.wfm_send(wfm_samples, marker1, marker2, wfm_pn, clock)
+				awg.wfm_import(wfm_fn, wfm_pn, 'WFM')
+				#awg.send_waveform(wfm_samples, marker, marker, wfm_pn, clock)
+				#awg_say('MMEM:IMP "%s", "%s", WFM'%(wfm_fn, wfm_pn)) # import wfm into list
+				# assign waveform to channel/time slot
+			awg.wfm_assign(wfm_channel, ti+1, wfm_fn)
+			#awg_say('SEQ:ELEM%d:WAV%d "%s"'%(ti+1, wfm_channel, wfm_fn))
+			if(loop): awg.set_seq_loop(ti+1, np.infty) # awg_say('SEQ:ELEM%d:LOOP:INF '%(ti, int(loop)))
+			p.iterate()
+			#awg_say('SEQ:ELEM%d:JTAR:TYPE %s'%(ti, 'IND'))
+			#awg_say('SEQ:ELEM%d:JTAR:IND %d'%(ti, 1))
+			#awg_say('SEQ:ELEM%d:GOTO:IND %d'%(ti, 1))
+
+	if(reset):
+		# set up looping
+		awg.set_seq_goto(len(ts), 1)
+		#awg_say('SEQ:ELEM%d:GOTO:STAT %d'%(len(ts), 1))
+		#awg_say('SEQ:ELEM%d:GOTO:IND %d'%(len(ts), 1))
+		
+		# enable channels
+		try:
+			time.sleep(0.1)
+			for channel in wfm_channels:
+				ch_state = 0
+				while(ch_state == 0):
+					#getattr(awg, 'set_ch%d_status'%channel)('on')
+					#awg.wait()
+					#if(getattr(awg, 'get_ch%d_status'%channel)() != 'on'):
+					ch_state = int(awg_ask(':OUTP%d 1; *WAI; :OUTP%d?'%(channel, channel)))
+					if(ch_state != 1):
+						print 'failed to enable awg channel %d.'%channel
+						time.sleep(0.1)
+		except:
+			pass
+	
+		# start awg
+		awg.run()
+		awg.wait(10,False)
+	qt.mend()
+	#'SEQ:ELEM%d:LOOP:COUN %d'%(idx, 1)
+	#'SEQ:ELEM%d:TWAI %d'%(idx, 1)
+
+def awg_update_2D_sequence(ts, wfm_func, sample, loop = False, drive = 'c:', path = '\\waveforms', reset = True, marker=None): #@andre20150318
+	'''
+		set awg to sequence mode and push a number of waveforms into the sequencer
+		
+		wfm_func has to be a tuple of two wfms, for ch1 resp ch2
+		
+		for the 6GS/s AWG, the waveform length must be divisible by 64
+		for the 1.2GS/s AWG, it must be divisible by 4
+	'''
+	qt.mstart()
+	awg = sample.get_awg()
+	clock = sample.get_clock()
+	
+	#awg_say = awg._ins._visainstrument.write
+	#awg_ask = awg._ins._visainstrument.ask
+	# create new sequence
+	if(reset):
+		awg.set_runmode('SEQ')
+		awg.set_seq_length(0) #awg_say('SEQ:LENG %d'%(0)) # clear sequence
+		awg.set_seq_length(len(ts)) #awg_say('SEQ:LENG %d'%(len(ts))) # create empty sequence
+
+	# update all channels and times
+
+	wfm_samples_prev = [None,None]
+	wfm_fn = [None,None]
+	wfm_pn = [None,None]
+	p = qkit.Progress_Bar.Progress_Bar(len(ts)*2)
+	for ti in range(len(ts)):
+		qt.msleep()
+		t = ts[ti]
+		# filter duplicates
+		wfm_samples = wfm_func(t)
+		
+		for chan in (0,1):
+			if(np.array_equal(wfm_samples[chan], wfm_samples_prev[chan])):
+				# waveform was seen before: just set it in the sequencer
+				pass
+			else:
+				# waveform is new: upload to awg
+				wfm_samples_prev[chan] = wfm_samples[chan]
+				wfm_fn[chan] = 'ch%d_t%05d'%(chan+1, ti) # filename is kept until changed
+				wfm_pn[chan] = '%s%s\\%s'%(drive, path, wfm_fn[chan])
+				# this results in "low" when the awg is stopped and "high" when it is running 
+				
+				if marker==None:
+					marker1 = np.array([int(0) for j in range(len(wfm_samples[chan]))])
+					marker2 = np.array([int(0) for j in range(len(wfm_samples[chan]))])
+					#make the markers at least 3 ns
+					#marker samples
+					m_sam = int(clock*3e-9)
+					for j in range(m_sam):
+						marker1[1+j] = int(1)
+						marker2[len(wfm_samples[chan])-1-j] = int(1)
+				else: #or set your own markers
+					if marker == "none":
+						marker1 = np.array([int(0) for j in range(len(wfm_samples[chan]))])
+						marker2 = np.array([int(0) for j in range(len(wfm_samples[chan]))])
+					else:
+						marker1,marker2 = marker[chan]
+				
+				#this was the old marker def
+	##				marker = np.ones(wfm_samples.shape, np.int)
+	##				marker[-1] = 0
+				awg.wfm_send(wfm_samples[chan], marker1, marker2, wfm_pn[chan], clock)
+				awg.wfm_import(wfm_fn[chan], wfm_pn[chan], 'WFM')
+				#awg.send_waveform(wfm_samples, marker, marker, wfm_pn, clock)
+				#awg_say('MMEM:IMP "%s", "%s", WFM'%(wfm_fn, wfm_pn)) # import wfm into list
+				# assign waveform to channel/time slot
+			awg.wfm_assign(chan+1, ti+1, wfm_fn[chan])
+			#awg_say('SEQ:ELEM%d:WAV%d "%s"'%(ti+1, wfm_channel, wfm_fn))
+			if(loop): awg.set_seq_loop(ti+1, np.infty) # awg_say('SEQ:ELEM%d:LOOP:INF '%(ti, int(loop)))
+			p.iterate()
+			#awg_say('SEQ:ELEM%d:JTAR:TYPE %s'%(ti, 'IND'))
+		#awg_say('SEQ:ELEM%d:JTAR:IND %d'%(ti, 1))
+		#awg_say('SEQ:ELEM%d:GOTO:IND %d'%(ti, 1))
+	print "Done: ",time.ctime()
+
+	if(reset):
+		# set up looping
+		awg.set_seq_goto(len(ts), 1)
+		#awg_say('SEQ:ELEM%d:GOTO:STAT %d'%(len(ts), 1))
+		#awg_say('SEQ:ELEM%d:GOTO:IND %d'%(len(ts), 1))
+		
+		# enable channels
+		try:
+			time.sleep(0.1)
+			for channel in wfm_channels:
+				ch_state = 0
+				while(ch_state == 0):
+					#getattr(awg, 'set_ch%d_status'%channel)('on')
+					#awg.wait()
+					#if(getattr(awg, 'get_ch%d_status'%channel)() != 'on'):
+					ch_state = int(awg_ask(':OUTP%d 1; *WAI; :OUTP%d?'%(channel, channel)))
+					if(ch_state != 1):
+						print 'failed to enable awg channel %d.'%channel
+						time.sleep(0.1)
+		except:
+			pass
+	
+		# start awg
+		awg.run()
+		awg.wait(10,False)
+	qt.mend()
+	#'SEQ:ELEM%d:LOOP:COUN %d'%(idx, 1)
+	#'SEQ:ELEM%d:TWAI %d'%(idx, 1)
