a069cc1446b98a958acc82fb1570469837b96401
Schneider1 <@>
2015-04-22 09:40:24 +0000

    improved save and get_all

--- measure/samples_class.py
+++ measure/samples_class.py
@@ -4,6 +4,7 @@ import time
 import pickle
 import qt
 import logging
+import os, copy, types
 
 class Sample(object):
 	'''
@@ -22,6 +23,7 @@ class Sample(object):
 		self.awg = None
 		self.qubit_mw_src = None
 		self.iq_frequency = 0
+		self.exc_T = None
 		
 	def update_instruments(self):
 		'''
@@ -30,10 +32,14 @@ class Sample(object):
 		- qubit_mw_src power
 		- qubit_mw_src f01-iq_frequency
 		'''
-		if (awg == None or qubit_mw_src == None):
-			logging.error(__name__ + ' : awg or qubit_mw_src not defined')
+		if (awg == None):
+			logging.error(__name__ + ' : awg not defined')
 		else:
 			self.awg.set_clock(self.clock)
+			
+		if (qubit_mw_src == None):
+			logging.error(__name__ + ' : qubit_mw_src not defined')
+		else:
 			self.qubit_mw_src.set_frequency(self.f01-self.iq_frequency)
 			self.qubit_mw_src.set_power(self.mw_power)
 			
@@ -42,6 +48,12 @@ class Sample(object):
 	
 	def get_iq_frequency(self):
 		return self.iq_frequency
+		
+	def set_exc_T(self,exc_T):
+		self.exc_T = exc_T
+	
+	def get_exc_T(self):
+		return self.exc_T
 	
 	def set_qubit_mw_src(self,qubit_mw_src):
 		self.qubit_mw_src = qubit_mw_src
@@ -114,21 +126,37 @@ class Sample(object):
 	
 	def get_all(self):
 		msg = ""
-		for key in sorted(self.__dict__):   				# There is a block readable by humans
-			msg+= str(key) + ":	" + str(self.__dict__[key])+"\n"
+		copydict = copy.copy(self.__dict__)
+		if type(copydict['awg']) == types.InstanceType: 
+			copydict['awg']="Instrument "+copydict['awg'].get_name()
+		if type(copydict['qubit_mw_src']) == types.InstanceType: 
+			copydict['qubit_mw_src'] = "Instrument "+copydict['qubit_mw_src'].get_name()
+			
+		for key in sorted(copydict):   				# There is a block readable by humans
+			msg+= str(key) + ":	" + str(copydict[key])+"\n"
 			
 		print msg
 	
 	def save(self,filename=None):
+		if not os.path.exists(os.path.join(qt.config.get('datadir'),time.strftime("%Y%m%d"))):
+				os.makedirs(os.path.join(qt.config.get('datadir'),time.strftime("%Y%m%d")))
+				
 		if filename==None:
-			filename=time.strftime("%Y%m%d%H%M%S.sample")
+			filename=time.strftime("%H%M%S.sample")
 		msg = ""
-		for key in sorted(self.__dict__):   				# There is a block readable by humans
-			msg+= str(key) + ":	" + str(self.__dict__[key])+"\n"
-			
+		copydict = copy.copy(self.__dict__)
+		if type(copydict['awg']) == types.InstanceType: 
+			copydict['awg']=copydict['awg'].get_name()
+		if type(copydict['qubit_mw_src']) == types.InstanceType: 
+			copydict['qubit_mw_src'] = copydict['qubit_mw_src'].get_name()
+		
+		for key in sorted(copydict):   				# There is a block readable by humans
+			msg+= str(key) + ":	" + str(copydict[key])+"\n"
+		
 		msg+="\n\n\n<PICKLE PACKET BEGINS HERE>\n" # A Separator
-		msg+=pickle.dumps(self.__dict__,protocol=1) # And a block which can be easily converted back to a dict
-		filehandle=open(filename,'w+')
+		msg+=pickle.dumps(copydict,protocol=1) # And a block which can be easily converted back to a dict
+		filehandle=open(os.path.join(qt.config.get('datadir'),time.strftime("%Y%m%d"),filename),'w+')
+		print "Saved to "+os.path.join(qt.config.get('datadir'),time.strftime("%Y%m%d"),filename)
 		filehandle.write(msg)
 		filehandle.close()
 	
@@ -136,7 +164,13 @@ class Sample(object):
 		
 		
 	def load(self, filename):
+		if not os.path.isabs(filename):
+			filename = os.path.join(qt.config.get('datadir'),filename)
 		filehandle=open(filename,'r')
 		self.__dict__ = pickle.loads(filehandle.read().split("<PICKLE PACKET BEGINS HERE>\n")[1])
 		#print pickle.loads(filehandle.read().split("<PICKLE PACKET BEGINS HERE>\n")[1]).__dict__
 		filehandle.close()
+		if self.awg != None:
+			self.awg = qt.instruments.get(self.awg)
+		if self.qubit_mw_src != None:
+			self.qubit_mw_src = qt.instruments.get(self.qubit_mw_src)
