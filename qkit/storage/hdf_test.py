6593b2b7f1fabb1481616b3c5b59427a57f42fbd
rotzinger <@>
2015-06-20 22:27:40 +0000

    added __init__ and hdf_test.py

---
+++ qkit/storage/hdf_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 00:16:06 2015
+
+@author: hrotzing
+"""
+import sys
+sys.path.append("/Users/hrotzing/pik/devel/python/qkit")
+#import qkit
+from qkit.storage import hdf_lib 
+print dir(hdf_lib)
