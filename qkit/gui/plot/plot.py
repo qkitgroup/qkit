74c70637050c0e50a373108f26209b855467b0c9
mpfirrmann <@>
2015-07-16 19:39:35 +0000

    Next try to get hdf-viewer to work

--- qkit/gui/plot/plot.py
+++ qkit/gui/plot/plot.py
@@ -1,4 +1,29 @@
+## MP 06/16/15:
+## not tested, since I could not install h5py
+
+import os
 from subprocess import Popen
+
+## the os.path.abspath gives the correct path to main.py, whith the working 
+## directory being /qkit (as it is in our case)
+qviewkit_main = os.path.join('qkit','gui','qviewkit','main.py')
+qviewkit_main_path = os.path.abspath(qviewkit_main)
+
+def plot_hdf(filepath, datasets=[]):
+	filepath = os.path.abspath(filepath)
+	arguments = ' -f '+filepath+' '
+	if datasets:
+		arguments += '-ds '
+		for dataset in datasets:
+			arguments += dataset+','
+     	open_main = 'python ' + qviewkit_main_path + arguments
+
+## As far as I understood, the encode('string-escape') does the same as \r 
+     	open_main.encode('string-escape')
+	Popen(open_main, shell=True)
+
+## "old":
+"""
 def plot_hdf(filepath, datasets=[]):
     append_string = r'-f '+filepath + ' '
     if datasets:
@@ -6,3 +31,4 @@ def plot_hdf(filepath, datasets=[]):
         for dataset in datasets:
             append_string += str(dataset)+','
     Popen(r'python C:\qtlab\qkit\qkit\gui\qviewkit\main.py '+append_string[:-1], shell=True)
+"""
