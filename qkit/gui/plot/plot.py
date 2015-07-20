67350205db79f4e1dea8a889ca5476caa434e0dd
mpfirrmann <@>
2015-07-20 08:09:59 +0000

    hdf live plot

--- qkit/gui/plot/plot.py
+++ qkit/gui/plot/plot.py
@@ -5,7 +5,6 @@ def plot(h5_filepath, datasets=[], refresh = 5, live = True, echo = False):
     for s in datasets: ds+=s+","
     ds = ds.strip(",")
 
-    print ds
     cmd = "python"
     cmd += " -m qkit.gui.qviewkit.main"
     options =  " -f " + h5_filepath.encode("string-escape")
@@ -13,7 +12,7 @@ def plot(h5_filepath, datasets=[], refresh = 5, live = True, echo = False):
     options += " -rt "+ str(refresh)
     if live:
         options += " -live "
-    print cmd,options
+
     if echo:
         print "Qviewkit open cmd: "+ cmd + options
         print Popen(cmd+options, shell=True, stdout=PIPE).stdout.read()
