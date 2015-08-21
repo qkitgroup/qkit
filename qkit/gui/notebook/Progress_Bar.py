c81ad9691c4f573582ef8d0a226c54e5955c7e2f
Schneider1 <@>
2015-08-21 10:10:41 +0000

    Removed clear_output

--- qkit/gui/notebook/Progress_Bar.py
+++ qkit/gui/notebook/Progress_Bar.py
@@ -19,40 +19,35 @@ class Progress_Bar(object):
 		#create HTML progress bar
 		
 		self.divid = str(uuid.uuid4())
-		self.pb = HTML(
-		"""
-		<div style="border: 1px solid black; width:900px">
-		  <div id="%s" style="background-color:blue; width:0%%">&nbsp;</div>
-		</div> 
-		""" % self.divid)
 		
 		self.max_it = max_it
 		self.progr = 0
-		clear_output()
+		self.pb = HTML(
+		"""
+		<div id="%sh" style="border: 1px solid black; width:900px">
+		  <div id="%s" style="text-align: center; color:white; background-color:blue; width:0%%">&nbsp;</div>
+		</div> 
+		<div id="%st">(0/%i) Starting</div>
+		""" % (self.divid,self.divid,self.divid,max_it))
 		display(self.pb)
 		display(Javascript("$('div#%s').width('%i%%')" % (self.divid, 100*self.progr/self.max_it)))
-		print 'starting measurement...'
 		sys.stdout.flush()
 		self.starttime = time.time()
 		
 	def iterate(self):
 		self.progr += 1
-		clear_output(wait=True)
-		display(self.pb)
-		display(Javascript("$('div#%s').width('%i%%')" % (self.divid, 100*self.progr/self.max_it)))
-		print "(%i/%i) ETA: %s  Time elapsed: %s"%(self.progr,self.max_it,time.ctime(time.time() + float(time.time()-self.starttime)/self.progr * (self.max_it - self.progr)),time.strftime('%H:%M:%S',time.gmtime(time.time()-self.starttime)))
-		sys.stdout.flush()
+		display(Javascript("$('div#%s').width('%i%%');" % (self.divid, 100*self.progr/self.max_it)))
+		outp = "(%i/%i) &#10148;  ETA: %s &#10148; Time elapsed: %s"%(self.progr,self.max_it,time.ctime(time.time() + float(time.time()-self.starttime)/self.progr * (self.max_it - self.progr)),time.strftime('%H:%M:%S',time.gmtime(time.time()-self.starttime)))
+		display(Javascript("document.getElementById('%st').innerHTML = '%s';"%(self.divid,outp)))
 		
 		if self.progr == self.max_it:   #end
-			self.pb = HTML(
-			"""
-			<div style="border: 1px solid black; width:900px">
-			  <div id="%s" style="background-color:green; width:0%%">&nbsp;</div>
-			</div> 
-			""" % self.divid)   #switch to green
+			#Turn the status bar into green
+			display(Javascript("document.getElementById('%s').style.backgroundColor = 'green';"%self.divid))
+			#Delete all <div> containers
+			#display(Javascript("document.getElementById('%s').remove();"%self.divid)) #blue box
+			#display(Javascript("document.getElementById('%sh').remove();"%self.divid)) #frame
+			display(Javascript("document.getElementById('%st').remove();"%self.divid)) #text
 			
-			clear_output()
-			display(self.pb)
-			display(Javascript("$('div#%s').width('%i%%')" % (self.divid, 100)))
-			print "(%i/%i) ETA: %s  Time elapsed: %s"%(self.progr,self.max_it,time.ctime(time.time() + float(time.time()-self.starttime)/self.progr * (self.max_it - self.progr)),time.strftime('%H:%M:%S',time.gmtime(time.time()-self.starttime)))
-			sys.stdout.flush()
+			#Print status text into progress bar (not below)
+			display(Javascript("document.getElementById('%s').innerHTML = '%s';"%(self.divid,outp)))
+			
