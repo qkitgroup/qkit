bd86c60fd4e52ff1703b0f1018e5c49fb4194597
mpfirrmann <marco.pfirrmann@kit.edu>
2016-01-27 11:24:57 +0100

    create qkit-specific config

---
+++ qkit/config/environment.py
@@ -0,0 +1,29 @@
+import tempfile
+
+# create universal qkit-config. Every module in qkit can rely on these entries to exist
+# this is independent from qtlab
+cfg = {}
+cfg['datadir'] = tempfile.gettempdir()
+cfg['qtlab'] = False
+
+# if qtlab is used (qt_cfg exists and qt_cfg['qtlab']): qkit config entries are overridden by the qtlab ones
+try:
+    from lib.config import get_config
+    qt_cfg = get_config()
+    in_qt = qt_cfg.get('qtlab', False)
+except ImportError:
+    in_qt = False
+
+if in_qt:
+    for entry in qt_cfg:
+        if entry in cfg.iterkeys():
+            cfg[entry] = qt_cfg[entry]
+
+# there can also be a local config file for qkit
+try:
+    from qkit.config.local import cfg_local
+    for entry in cfg_local:
+        cfg[entry] = cfg_local[entry]
+
+except ImportError:
+    pass
