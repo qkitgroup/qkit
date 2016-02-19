f99d52186b2835204735ad5922ceca38eee40443
rotzinger <hannes.rotzinger@gmail.com>
2016-02-19 15:25:39 +0100

    changes to the hdf_dataset; added string datasets

---
+++ qkit/storage/hdf_constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 14:39:10 2016
+
+@author: hrotzing
+"""
+
+ds_types = {'coordinate':0,
+            'vector':1,
+            'matrix':2,
+            'box':3,
+            'txt':10,
+            'view':20,            
+            } 
