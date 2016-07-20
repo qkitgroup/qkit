b08c3d061cd32ac07ec878bed47eec08148b156c
mpfirrmann <marco.pfirrmann@kit.edu>
2016-07-20 11:02:29 +0200

    saves pic of hdf-box, color coded x vs y at midpoint of z; adapted ds_types

--- qkit/gui/plot/plot.py
+++ qkit/gui/plot/plot.py
@@ -7,6 +7,7 @@ import numpy as np
 import logging
 logging.basicConfig(level=logging.INFO)
 from qkit.config.environment import cfg
+from qkit.storage.hdf_constants import ds_types
 
 # this is for live-plots
 def plot(h5_filepath, datasets=[], refresh = 2, live = True, echo = False):
@@ -43,17 +44,17 @@ def plot(h5_filepath, datasets=[], refresh = 2, live = True, echo = False):
 
 
 # this is for saving plots
-def save_plots(h5_filepath, datasets=[], comment='', save_pdf=False):
+def save_plots(h5_filepath, comment='', save_pdf=False):
     """
     Save plots is a helper function to extract and save image plots from hdf-files
 
     """
-    h5p = h5plot(h5_filepath, datasets=[], comment=comment,  save_pdf=False)
+    h5plot(h5_filepath, comment=comment,  save_pdf=False)
 
 
 class h5plot(object):
 
-    def __init__(self,h5_filepath, datasets=[], comment='', save_pdf=False):
+    def __init__(self,h5_filepath, comment='', save_pdf=False):
 
         self.comment = comment
         self.save_pdf = save_pdf
@@ -62,6 +63,7 @@ class h5plot(object):
         self.filedir  = os.path.dirname(filepath)   #return directory component of the given pathname, here filepath
 
         self.image_dir = os.path.join(self.filedir,'images')
+        print self.image_dir
         try:
             os.mkdir(self.image_dir)
         except OSError:
@@ -72,132 +74,153 @@ class h5plot(object):
         self.hf = hdf_lib.Data(path=h5_filepath)
 
         # check for datasets
-        if datasets:
-            logging.info(" -> plotting given datasets:" +str(datasets))
-            for i,key in enumerate(datasets):
-
-                split_key=key.split('/')
-                if len(split_key) == 1:
-                    key = "/entry/data0/"+split_key[0]
-                    self.plt_ds(key)
-                if len(split_key) == 2:
-                    key = "/entry/"+split_key[0]+"/"+split_key[1]
-                    if split_key[0] == 'views':
-                        self.plt_views(key)
-                    else:
-                        self.plt_ds(key)
-        else:
-            for i, pentry in enumerate(self.hf['/entry'].keys()):
-                # do not plot analysis-datasets
-                if pentry != 'analysis0':
-                    key='/entry/'+pentry
-                    for j, centry in enumerate(self.hf[key].keys()):
-                        key='/entry/'+pentry+"/"+centry
-                        if pentry == 'views':
-                            self.plt_views(key)
-                        else:
-                            self.plt_ds(key)
+        for i, pentry in enumerate(self.hf['/entry'].keys()):
+            key='/entry/'+pentry
+            for j, centry in enumerate(self.hf[key].keys()):
+                self.key='/entry/'+pentry+"/"+centry
+                self.ds = self.hf[self.key]
+                if self.ds.attrs.get('save_plot', True):
+                    self.plt()
 
         #close hf file
         self.hf.close()
         print 'Plots saved in', self.image_dir
 
-    def plt_ds(self,dataset):
-        try:
-            logging.info(" -> plotting dataset: "+str(dataset))
+    def plt(self):
+        logging.info(" -> plotting dataset: "+str(self.ds.attrs.get('name')))
 
-            ds=self.hf[dataset]
-            x_ds_url = ds.attrs.get('x_ds_url','')
-            y_ds_url = ds.attrs.get('y_ds_url','')
+        self.ds_type = self.ds.attrs.get('ds_type', '')
+        self.x_ds_url = self.ds.attrs.get('x_ds_url','')
+        self.y_ds_url = self.ds.attrs.get('y_ds_url','')
+        self.z_ds_url = self.ds.attrs.get('z_ds_url','')
 
-            " if no x or y references exist skip the dataset, e.g. for timestamps"
-            if not x_ds_url:
-                 return
+        self.fig, self.ax = plt.subplots(figsize=(20,10))
 
-            x_ds = self.hf[x_ds_url]
+        if self.ds_type == ds_types['coordinate']:
+            #self.plt_coord()
+            return
+        elif self.ds_type == ds_types['vector']:
+            self.plt_vector()
+        elif self.ds_type == ds_types['matrix']:
+            self.plt_matrix()
+        elif self.ds_type == ds_types['box']:
+            self.plt_box()
+        elif self.ds_type == ds_types['txt']:
+            #self.plt_txt()
+            return
+        elif self.ds_type == ds_types['view']:
+            #self.plt_view()
+            return
+        else:
+            return
 
-            x_label = x_ds.attrs.get('name','_xname_')+' / '+x_ds.attrs.get('unit','_xunit_')
-            ds_label = ds.attrs.get('name','_name_')+' / '+ds.attrs.get('unit','_unit_')
+        self.ax.set_xlabel(self.x_label)
+        self.ax.set_ylabel(self.y_label)
+        self.ax.xaxis.label.set_fontsize(20)
+        self.ax.yaxis.label.set_fontsize(20)
+        self.ax.ticklabel_format(useOffset=False)
+        for i in self.ax.get_xticklabels():
+            i.set_fontsize(16)
+        for i in self.ax.get_yticklabels():
+            i.set_fontsize(16)
+        self.fig.tight_layout()
 
-            # Hack to detect (and not plot) coordinate-datasets
-            if x_ds == ds:
-                return
+        save_name = str(os.path.basename(self.filedir))[0:6] + '_' + self.key.replace('/entry/','').replace('/','_')
+        if self.comment:
+            save_name = save_name+'_'+self.comment
+        image_path = str(os.path.join(self.image_dir,save_name))
 
-            fig, ax = plt.subplots(figsize=(20,10))
+        if self.save_pdf:
+            self.fig.savefig(image_path+'.pdf')
+        self.fig.savefig(image_path+'.png')
 
-            if len(ds.shape)==1:
-            #checking the shape is a little hack to save plots from earlier .h5 files without proper metadata settings
-                """
-                dataset is only one-dimensional
-                print data vs. x-coordinate
-                """
-                logging.info("ds is one dimensional.")
-
-                data_y = ds
-                y_label = ds_label
-
-                ax.plot(x_ds,data_y[0:len(x_ds)], '-')   #JB: avoid crash after pressing the stop button when arrays are of different lengths
-
-            elif len(ds.shape)>=2:
-                """
-                dataset is two-dimensional
-                print data color-coded y-coordinate vs. x-coordinate
-                """
-                logging.info("ds is two dimensional.")
-                if not y_ds_url:
-                    return
-                y_ds = self.hf[y_ds_url]
-                data_y = np.array(y_ds)
-                y_label = y_ds.attrs.get('name','_yname_')+' / '+y_ds.attrs.get('unit','_yunit_')
-
-                data_z = np.array(ds)
-                #slice the value-box in the midpoint of the z-axis
-                if len(ds.shape)==3:
-                    data_z = data_z[:,:,data_y.shape[2]/2]
-
-                xmin = x_ds.attrs.get('x0',0)
-                xmax = xmin+x_ds.attrs.get('dx',1)*x_ds.shape[0]
-                ymin = y_ds.attrs.get('x0',0)
-                ymax = ymin+y_ds.attrs.get('dx',1)*y_ds.shape[0]
-
-
-                cax = ax.imshow(data_z.T, aspect='auto', extent=[xmin,xmax,ymin,ymax], origin = 'lower', interpolation='none')
-                cbar = fig.colorbar(cax)
-                cbar.ax.set_ylabel(ds_label)
-                cbar.ax.yaxis.label.set_fontsize(20)
-                for i in cbar.ax.get_yticklabels():
-                    i.set_fontsize(16)
-            else:
-                pass
-
-            ax.set_xlabel(x_label)
-            ax.set_ylabel(y_label)
-            ax.xaxis.label.set_fontsize(20)
-            ax.yaxis.label.set_fontsize(20)
-            ax.ticklabel_format(useOffset=False)
-            for i in ax.get_xticklabels():
-                i.set_fontsize(16)
-            for i in ax.get_yticklabels():
-                i.set_fontsize(16)
-            fig.tight_layout()
-
-            save_name = str(os.path.basename(self.filedir))[0:6] + '_' + dataset.replace('/entry/','').replace('/','_')
-            if self.comment:
-                save_name = save_name+'_'+self.comment
-            image_path = str(os.path.join(self.image_dir,save_name))
-
-            if self.save_pdf:
-                fig.savefig(image_path+'.pdf')
-            fig.savefig(image_path+'.png')
-
-            plt.close()
+        plt.close()
+        """
         except Exception as e:
             print "Exception in qkit/gui/plot/plot.py"
             print e
+        """
 
-    def plt_views(self,key):
-            # not (yet?) implemented. we'll see ...
-            pass
+    def plt_vector(self):
+        """
+        dataset is only one-dimensional
+        print data vs. x-coordinate
+        """
+        self.x_ds = self.hf[self.x_ds_url]
+
+        self.x_label = self.x_ds.attrs.get('name','_xname_')+' / '+self.x_ds.attrs.get('unit','_xunit_')
+        self.ds_label = self.ds.attrs.get('name','_name_')+' / '+self.ds.attrs.get('unit','_unit_')
+        self.data_y = np.array(self.ds)
+        self.y_label = self.ds_label
+        self.ax.plot(self.x_ds,self.data_y[0:len(self.x_ds)], '-')   #JB: avoid crash after pressing the stop button when arrays are of different lengths
+
+    def plt_matrix(self):
+        """
+        dataset is two-dimensional
+        print data color-coded y-coordinate vs. x-coordinate
+        """
+
+        self.x_ds = self.hf[self.x_ds_url]
+        self.y_ds = self.hf[self.y_ds_url]
+
+        self.x_label = self.x_ds.attrs.get('name','_xname_')+' / '+self.x_ds.attrs.get('unit','_xunit_')
+        self.y_label = self.y_ds.attrs.get('name','_yname_')+' / '+self.y_ds.attrs.get('unit','_yunit_')
+        self.ds_label = self.ds.attrs.get('name','_name_')+' / '+self.ds.attrs.get('unit','_unit_')
+
+        self.data = np.array(self.ds)
+
+        self.xmin = self.x_ds.attrs.get('x0',0)
+        self.xmax = self.xmin+self.x_ds.attrs.get('dx',1)*self.x_ds.shape[0]
+        self.ymin = self.y_ds.attrs.get('x0',0)
+        self.ymax = self.ymin+self.y_ds.attrs.get('dx',1)*self.y_ds.shape[0]
+
+
+        self.cax = self.ax.imshow(self.data.T, aspect='auto', extent=[self.xmin,self.xmax,self.ymin,self.ymax], origin = 'lower', interpolation='none')
+        self.cbar = self.fig.colorbar(self.cax)
+        self.cbar.ax.set_ylabel(self.ds_label)
+        self.cbar.ax.yaxis.label.set_fontsize(20)
+        for i in self.cbar.ax.get_yticklabels():
+            i.set_fontsize(16)
+    
+    def plt_box(self):
+        """
+        dataset is two-dimensional
+        print data color-coded y-coordinate vs. x-coordinate
+        """
+
+        self.x_ds = self.hf[self.x_ds_url]
+        self.y_ds = self.hf[self.y_ds_url]
+        self.z_ds = self.hf[self.z_ds_url]
+
+        self.x_label = self.x_ds.attrs.get('name','_xname_')+' / '+self.x_ds.attrs.get('unit','_xunit_')
+        self.y_label = self.y_ds.attrs.get('name','_yname_')+' / '+self.y_ds.attrs.get('unit','_yunit_')
+        self.ds_label = self.ds.attrs.get('name','_name_')+' / '+self.ds.attrs.get('unit','_unit_')
+
+        self.nop = self.z_ds.shape[0]
+        self.data = np.array(self.ds)[:,:,self.nop/2]
+
+        self.xmin = self.x_ds.attrs.get('x0',0)
+        self.xmax = self.xmin+self.x_ds.attrs.get('dx',1)*self.x_ds.shape[0]
+        self.ymin = self.y_ds.attrs.get('x0',0)
+        self.ymax = self.ymin+self.y_ds.attrs.get('dx',1)*self.y_ds.shape[0]
+
+
+        self.cax = self.ax.imshow(self.data.T, aspect='auto', extent=[self.xmin,self.xmax,self.ymin,self.ymax], origin = 'lower', interpolation='none')
+        self.cbar = self.fig.colorbar(self.cax)
+        self.cbar.ax.set_ylabel(self.ds_label)
+        self.cbar.ax.yaxis.label.set_fontsize(20)
+        for i in self.cbar.ax.get_yticklabels():
+            i.set_fontsize(16)
+
+    def plt_coord(self):
+        # not (yet?) implemented. we'll see ...
+        return
+    def plt_txt(self):
+        # not (yet?) implemented. we'll see ...
+        return
+    def plt_view(self):
+        # not (yet?) implemented. we'll see ...
+        return
 
 
 if __name__ == "__main__":
@@ -206,16 +229,11 @@ if __name__ == "__main__":
     description="plot.py hdf and matplotlib-based plotting of datasets / KIT 2015")
 
     parser.add_argument('-f','--file',required=True,type=str, help='hdf/h5 filename to open')
-    parser.add_argument('-ds','--datasets', type=str, help='(optional) datasets opened by default')
     parser.add_argument('-c', '--comment', type=str, help='(optional) comment to append at filenames')
     parser.add_argument('-pdf','--save-pdf', default=False,action='store_true', help='(optional) save default plots')
 
     args=parser.parse_args()
-    # split the dataset string into a list
-    datasets=[]
-    if args.datasets:
-        dss=args.datasets.split(',')
-        for ds in dss: datasets.append(ds)
+
     # get the full path
     filepath= os.path.abspath(args.file)
-    save_plots(filepath, datasets=datasets,comment=args.comment,save_pdf=args.save_pdf)
+    save_plots(filepath,comment=args.comment,save_pdf=args.save_pdf)
