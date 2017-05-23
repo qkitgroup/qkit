d874a46f50b6718323869eb64c371cae45886c66
mpfirrmann <marco.pfirrmann@kit.edu>
2017-05-23 13:28:44 +0200

    measurement_class: enable save and load

--- qkit/measure/measurement_class.py
+++ qkit/measure/measurement_class.py
@@ -1,110 +1,101 @@
- # MP, AS @ KIT 05/2017
+# MP, AS @ KIT 05/2017
 # Class to collect all information about a single measurement
 
-import time
 import json
-import logging
 import os, copy, types
 import numpy as np
+import qt
 from qkit.measure.samples_class import QkitJSONEncoder
-               
+from qkit.measure.samples_class import Sample
+
 class Measurement(object):
     '''
-    Measurement Class to define general information about the specific measurement
+    Measurement class to define general information about the specific measurement.
     '''
     def __init__(self):
-        self._experimentalist = 'fluxon'
-        self._name = ''
-        self._setup = ''
-        self._uuid = int(time.time())
-        self._hdf_filename = ''
-        self._web_visible = True
-        self._rating = 3
-        self._analyzed = False
-        self._measurement_type = ''
-        self._measurement_func = ''
-        self._x_axis = ''
-        self._y_axis = ''
-        self._z_axis = ''
-        
-    ####
-    # GETTER / SETTER
-    ####
-        
-    def get_experimentalist(self):
-        return self._experimentalist
-        
-    def set_experimentalist(self, exp):
-        self._experimentalist = exp
+        self.uuid = None
+        self.hdf_relpath = ''
+        self.sample = None
+        self.web_visible = True
+        self.rating = 3
+        self.analyzed = False
+        self.measurement_type = ''
+        self.measurement_func = ''
+        self.x_axis = ''
+        self.y_axis = ''
+        self.z_axis = ''
 
-    def get_name(self):
-        return self._name
-        
-    def set_name(self, name):
-        self._name = name
+    def get_all(self):
+        """
+        Returns printable, organized string of all attributes. The 'real' "Measurement" attributes are in front, followed by the inherited sample attributes.
+        The function is mostly used for saving in .h5.
+        """
+        copydict = copy.copy(self.__dict__)
+        sample = copydict.pop('sample', None)
 
-    def get_setup(self):
-        return self._setup
+        ret = ''
+        for key, value in sorted(copydict.items()):
+            ret += str(key)+': '+str(value)+'\n'
+
+        if sample:
+            ret += 'sample:\n'
+            sample_dict = sample.__dict__
+            for key, value in sorted(sample_dict.items()):
+                ret += '\t'+str(key)+': '+str(value)+'\n'
+        return ret.rstrip()
+
+    def save(self,filepath=None):
+        '''
+        Save sample object in the same directory as the measurement .h5 file or in the given filepath.
+        '''
         
-    def set_setup(self, setup):
-        self._setup = setup
-        
-    def get_uuid(self):
-        return self._uuid
-        
-    def set_uuid(self, uuid):
-        self._uuid = uuid
-        
-    def get_hdf_filename(self):
-        return self._hdf_filename
-        
-    def set_hdf_filename(self, hdf_filename):
-        self._hdf_filename = hdf_filename
-        
-    def get_web_visible(self):
-        return self._web_visible
-        
-    def set_web_visible(self, web_visible):
-        self._web_visible = web_visible
-        
-    def get_rating(self):
-        return self._rating
-        
-    def set_rating(self, rating):
-        self._rating = rating
-        
-    def get_analyzed(self):
-        return self._analyzed
-        
-    def set_analyzed(self, analyzed):
-        self._analyzed = analyzed
-        
-    def get_measurement_type(self):
-        return self._measurement_type
-        
-    def set_measurement_type(self, measurement_type):
-        self._measurement_type = measurement_type
-        
-    def get_measurement_func(self):
-        return self._measurement_func
-        
-    def set_measurement_func(self, measurement_func):
-        self._measurement_func = measurement_func
-        
-    def get_x_axis(self):
-        return self._x_axis
-        
-    def set_x_axis(self, x_axis):
-        self._x_axis = x_axis
-        
-    def get_y_axis(self):
-        return self._y_axis
-        
-    def set_y_axis(self, y_axis):
-        self._y_axis = y_axis
-        
-    def get_z_axis(self):
-        return self._z_axis
-        
-    def set_z_axis(self, z_axis):
-        self._z_axis = z_axis        
+        if filepath:
+            filepath=filepath
+        else:
+            filepath = os.path.join(qt.config.get('datadir'),self.hdf_relpath.replace('.h5', '.measurement'))
+
+        copydict = copy.copy(self.__dict__)
+
+        """
+        Sample entry has to be converted to JSON first, to save information about datatype.
+        """
+        if copydict['sample']:
+            copydict['sample'] = copydict['sample'].__dict__ # change entry to dict to make it readable for JSONEncoder
+        with open(filepath,'w+') as filehandler:
+            json.dump(obj=copydict, fp=filehandler, cls=QkitJSONEncoder, indent = 4, sort_keys=True)
+
+    def load(self, filename):
+        '''
+        Load sample keys and entries to current sample instance.
+        '''
+
+        if not os.path.isabs(filename):
+            filename = os.path.join(qt.config.get('datadir'),filename)
+
+        with open(filename) as filehandle:
+            self.__dict__ = json.load(filehandle)
+
+        copydict = copy.copy(self.__dict__)
+        for entry in sorted(copydict):
+            if type(copydict[entry]) == types.DictType:
+                if copydict[entry].has_key('dtype') and copydict[entry].has_key('content'): # non JSON standard, special handling
+                    if copydict[entry]['dtype'] == 'ndarray': self.__dict__[entry] = np.array(copydict[entry]['content'])
+                    if copydict[entry]['dtype'] == 'instance':  self.__dict__[entry] = qt.instruments.get(copydict[entry]['content'])
+            """
+            A "Sample" entry itself is a JSON encoded string of all attributes of the "Sample"-class.
+            Such a decoded "Sample"-object is created here.
+            """
+            if entry == 'sample':
+                s = Sample()
+                s.__dict__ = copydict[entry]
+
+                sample_dict = copy.copy(s.__dict__)
+                for entry_s in sorted(sample_dict):
+                    if type(sample_dict[entry_s]) == types.DictType:
+                        if sample_dict[entry_s].has_key('dtype') and sample_dict[entry_s].has_key('content'): # non JSON standard, special handling
+                            if sample_dict[entry_s]['dtype'] == 'ndarray': s.__dict__[entry_s] = np.array(sample_dict[entry_s]['content'])
+                            if copydict[entry][entry_s]['dtype'] == 'instance':  s.__dict__[entry_s] = qt.instruments.get(sample_dict[entry_s]['content'])
+                """
+                The created "Sample"-object is an attribute of the measurement object.
+                """
+                self.__dict__[entry] = s
