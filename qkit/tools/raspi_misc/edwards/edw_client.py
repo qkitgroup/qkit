5248865e7c7fa122c2ffc70560a9f0fc7872def3
JBraumueller <jochen.braumueller@kit.edu>
2017-05-23 13:17:49 +0200

    added folder 'tools/raspi_misc/edwards'

---
+++ qkit/tools/raspi_misc/edwards/edw_client.py
@@ -0,0 +1,39 @@
+# edw_client.py by JB@KIT 03/2015
+# test client for edw server
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import socket
+import sys
+
+HOST, PORT = 'ip-address', 9955
+data = 'jochen'
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(data + "\n")
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
+
