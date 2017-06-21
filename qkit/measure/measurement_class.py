b9659303d1d6d39abb7e43660311bb3e6c01a475
mpfirrmann <marco.pfirrmann@kit.edu>
2017-06-21 18:23:28 +0200

    fixed import bug from config

--- qkit/measure/measurement_class.py
+++ qkit/measure/measurement_class.py
@@ -25,8 +25,8 @@ class Measurement(object):
         self.y_axis = ''
         self.z_axis = ''
         self.instruments = None
-        self.user = cfg.get('user','John_Doe').strip().replace(" ","_")
-        self.run_id = cfg.get('run_id','NO_RUN')
+        self.user = qt.config.get('user','John_Doe').strip().replace(" ","_")
+        self.run_id = qt.config.get('run_id','NO_RUN')
 
     def get_JSON(self):
         """
