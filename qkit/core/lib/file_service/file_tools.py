e089b42bf833c77b1633fa4fb9aa4353b07e4145
Schneider1 <andre.schneider@kit.edu>
2018-01-12 17:38:23 +0100

    file_tools: make database generation threaded

--- qkit/core/lib/file_service/file_tools.py
+++ qkit/core/lib/file_service/file_tools.py
@@ -1,34 +1,41 @@
 import os
 import qkit
+import threading
 
 class store_db(object):
     def __init__(self):
         self.h5_db    = {}
         self.set_db  = {}
         self.measure_db = {}
+        self.lock = threading.Lock()
         self.create_database()
         
+        
     def __getitem__(self,key):
-        return self.h5_db.get(key,None)
+        with self.lock:
+            return self.h5_db.get(key,None)
     
     def get(self,key,args=None):
-        return self.h5_db.get(key,args)
+        with self.lock:
+            return self.h5_db.get(key,args)
 
-    def update_database(self):        
-        for root, dirs, files in os.walk(qkit.cfg['datadir']):
-            for f in files:
-                if f[-3:] == '.h5':
-                    uuid = f[:6]
-                    path = os.path.join(root,f)
-                    self.h5_db[uuid] = path
-                elif f[-3:] == 'set':
-                    uuid = f[:6]
-                    path = os.path.join(root,f)
-                    self.set_db[uuid] = path
-                elif f[-3:] == 'ent':
-                    uuid = f[:6]
-                    path = os.path.join(root,f)
-                    self.measure_db[uuid] = path
+    def update_database(self):
+        with self.lock:
+            for root, dirs, files in os.walk(qkit.cfg['datadir']):
+                for f in files:
+                    if f[-3:] == '.h5':
+                        uuid = f[:6]
+                        path = os.path.join(root,f)
+                        self.h5_db[uuid] = path
+                    elif f[-3:] == 'set':
+                        uuid = f[:6]
+                        path = os.path.join(root,f)
+                        self.set_db[uuid] = path
+                    elif f[-3:] == 'ent':
+                        uuid = f[:6]
+                        path = os.path.join(root,f)
+                        self.measure_db[uuid] = path
 
     def create_database(self):
-        self.update_database()
+        t = threading.Thread(target=self.update_database)
+        t.start()
