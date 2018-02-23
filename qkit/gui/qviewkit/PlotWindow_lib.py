4b48582870e6afa9537987e70bf40dc345beca8d
mpfirrmann <marco.pfirrmann@kit.edu>
2018-02-23 14:22:25 +0100

    PlotWindow_lib: integrated unified dataset info readout in 1D and 2D data. 1D view not yet generalized.

--- qkit/gui/qviewkit/PlotWindow_lib.py
+++ qkit/gui/qviewkit/PlotWindow_lib.py
@@ -27,7 +27,7 @@ from qkit.storage.hdf_constants import ds_types
 
 """ A few handy methods for label and scale """
 def _get_ds_url(ds,url):
-    return ds.attrs.get(url,None)
+    return ds.attrs.get(url,'_none_')
        
 def _get_ds(ds,ds_url):
     try:
@@ -37,13 +37,12 @@ def _get_ds(ds,ds_url):
     
 def _get_axis_scale(ds):
     # assumed that a axis is one dimensional:
-    """
-    x0 = ds[0]
-    dx = ds[1]-ds[0]
-    dsmax = ds[-1]
-    return (x0,dx,dsmax)
-    """
-    return (ds.attrs.get('x0', 0), ds.attrs.get('dx', 1))
+    try:
+        x0 = ds[0]
+        dx = ds[1]-ds[0]
+        return (x0,dx)
+    except Exception:
+        return (0,1)
 
 def _get_unit(ds):
     try:
@@ -57,7 +56,7 @@ def _get_name(ds):
     except Exception:
         return '_none_'
 
-def _get_all_ds_labels_units_scales_from(ds,ds_urls= []):
+def _get_all_ds_names_units_scales(ds,ds_urls= []):
     """ method to unify the way labels, units, and scales are defined """
     dss = []
     for ds_url in ds_urls:
@@ -65,18 +64,15 @@ def _get_all_ds_labels_units_scales_from(ds,ds_urls= []):
     # the last dataset is always the displayed data.
     dss.append(ds)
     
-    labels = []    
-    for ds in dss:
-        labels.append(_get_name(ds))
-    
-    units = []    
-    for ds in dss:
-        units.append(_get_unit(ds))
-    
+    names = []
+    units = []  
     scales = []
     for ds in dss:
-        scales.append(_get_axis_scale(ds)) # The data ds should in the end have no x0/dx entry
-    return dss, labels, units, scales
+        names.append(_get_name(ds))
+        units.append(_get_unit(ds))
+        scales.append(_get_axis_scale(ds))
+
+    return dss, names, units, scales
 
 def _display_1D_view(self,graphicsView):
     ds = self.ds
@@ -266,20 +262,21 @@ def _display_1D_data(self,graphicsView):
     handled individually.
     """
     if self.ds_type == ds_types['vector'] or self.ds_type == ds_types['coordinate'] or (self.ds_type == -1 and len(self.ds.shape) == 1): #last expresson is for old hdf-files
-        x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds,'x_ds_url'))
+        dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['x_ds_url'])
         
         # timestamps do (not?) have a x_ds_url in the 1d case. This is more a bug to be fixed in the
         # timstamp_ds part of qkit the resulting error is fixed here for now.
-        if x_axis_ds:
-            x_data = x_axis_ds[()][:self.ds.shape[-1]] #x_data gets truncated to y_data shape if neccessary
-        else:
-            x_data = [i for i in range(self.ds.shape[-1])]
+        try:
+            x_data =dss[0][()][:dss[1].shape[-1]] #x_data gets truncated to y_data shape if neccessary
+        except Exception:
+            x_data = [i for i in range(dss[1].shape[-1])]
+        y_data = dss[1][()]
 
     elif self.ds_type == ds_types['matrix'] or (self.ds_type == -1 and len(self.ds.shape) == 2): #last expresson is for old hdf-files
         """
         For a matrix type the data to be displayed on the x-axis is the y-axis (highest coordinate) of the ds
         """
-        x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds,'y_ds_url'))
+        dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['y_ds_url'])
 
         if self.TraceValueChanged:
             """
@@ -295,14 +292,14 @@ def _display_1D_data(self,graphicsView):
 
         self.TraceValue.setText(self._getXValueFromTraceNum(self.ds,self.TraceNum))
 
-        x_data = x_axis_ds[()][:self.ds.shape[-1]] #x_data gets truncated to y_data shape if neccessary
-        y_data = y_data[self.TraceNum]
+        x_data =dss[0][()][:dss[1].shape[-1]] #x_data gets truncated to y_data shape if neccessary
+        y_data = dss[1][()][self.TraceNum]
 
     elif self.ds_type == ds_types['box']:
         """
         For a box type the data to be displayed on the x-axis is the z-axis (highest coordinate) of the ds
         """
-        x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds,'z_ds_url'))
+        dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['z_ds_url'])
 
         if self.TraceXValueChanged:
             """
@@ -331,14 +328,14 @@ def _display_1D_data(self,graphicsView):
         self.TraceXValue.setText(self._getXValueFromTraceNum(self.ds,self.TraceXNum))
         self.TraceYValue.setText(self._getYValueFromTraceNum(self.ds,self.TraceYNum))
         
-        x_data = x_axis_ds[()][:self.ds.shape[-1]] #x_data gets truncated to y_data shape if neccessary
-        y_data = y_data[self.TraceXNum,self.TraceYNum,:]
+        x_data =dss[0][()][:dss[1].shape[-1]] #x_data gets truncated to y_data shape if neccessary
+        y_data = dss[1][()][self.TraceXNum,self.TraceYNum,:]
 
 
     # set the y data  to the decibel scale 
     if self.manipulation & self.manipulations['dB']:
         y_data = 20 *np.log10(y_data)
-        self.unit = 'dB'
+        units[1] = 'dB'
     
     # unwrap the phase
     if self.manipulation & self.manipulations['wrap']:
@@ -347,17 +344,9 @@ def _display_1D_data(self,graphicsView):
     # linearly correct the data    
     if self.manipulation & self.manipulations['linear']:
         y_data = y_data - np.linspace(y_data[0],y_data[-1],len(y_data))
-
-    """
-    The datasets on both axis are read out for name and units
-    """
-    x_axis_name = _get_name(x_axis_ds)
-    x_axis_unit = _get_unit(x_axis_ds)
-    y_axis_name = _get_name(self.ds)
-    y_axis_unit = self.unit
     
-    graphicsView.setLabel('left', y_axis_name, units=y_axis_unit)
-    graphicsView.setLabel('bottom', x_axis_name , units=x_axis_unit)
+    graphicsView.setLabel('left', names[1], units=units[1])
+    graphicsView.setLabel('bottom', names[0] , units=units[0])
 
     #if only one entry in the dataset --> point-style
     if self.ds.shape[-1]==1:
@@ -382,14 +371,14 @@ def _display_1D_data(self,graphicsView):
             xval = mousePoint.x()
             yval = mousePoint.y()
 
-            self.PointX.setText("X: %.6e %s"%(xval, x_axis_unit)) 
-            self.PointY.setText("Y: %.6e %s"%(yval, y_axis_unit)) 
-
+            self.PointX.setText("X: %.6e %s"%(xval, units[0])) 
+            self.PointY.setText("Y: %.6e %s"%(yval, units[1])) 
+            
             try:
                 self.data_coord=  "%e\t%e\t%e\t%e" % (xval, yval,self._last_x_pos-xval,xval/(self._last_x_pos-xval))
             except ZeroDivisionError:
                 pass
-
+            
             self._last_x_pos = xval
 
     self.proxy = pg.SignalProxy(plVi.scene().sigMouseMoved, rateLimit=15, slot=mouseMoved)
@@ -404,31 +393,22 @@ def _display_2D_data(self,graphicsView):
     (x0, y0) and a scale (dx, dy) for creating the axis tics and tic lables. This means it is not possible
     to display any non-linear scaled axis data.
     """
-    """
-    ds = self.ds
-    name = ds.attrs.get("name","_none_")
-    self.unit = ds.attrs.get("unit","_none_")
-    """
-    data = self.ds[()]
-    self.unit = _get_unit(self.ds)
-    # the datasets below are general axis datasets from the self.ds
 
     if self.ds_type == ds_types['matrix']:
         """
         The matrix ds-type only knows one 2d plotting option. x_ds on x- and y_ds on y-axis
         """
-        x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'x_ds_url'))
-        y_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'y_ds_url'))
+        dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['x_ds_url', 'y_ds_url'])
+        data = dss[2][()]
         
-        fill_x = self.ds.shape[0]
-        fill_y = self.ds.shape[1]        
+        fill_x = dss[2].shape[0]
+        fill_y = dss[2].shape[1]        
     if self.ds_type == ds_types['box']:
         """
         The box ds-type can be plotted from 3 different "viewing directions" (PlotType). Depending on its 
         setting the x- and y-axis are set.
         The ds-type box also has a z_ds_url.
         """
-        #z_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'z_ds_url'))
         if self.PlotTypeSelector.currentIndex() == 0: #y_ds on x-axis; z_ds on y-axis
             if self.TraceXValueChanged:
                 #calc trace number from entered value
@@ -437,12 +417,11 @@ def _display_2D_data(self,graphicsView):
                 self.TraceXSelector.setValue(self.TraceXNum)
                 self.TraceXValueChanged = False
             
-            x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'y_ds_url'))
-            y_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'z_ds_url'))
-            data = data[self.TraceXNum,:,:]
+            dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['y_ds_url', 'z_ds_url'])
+            data = dss[2][()][self.TraceXNum,:,:]
             
-            fill_x = self.ds.shape[1]
-            fill_y = self.ds.shape[2]
+            fill_x = dss[2].shape[1]
+            fill_y = dss[2].shape[2]
             
         if self.PlotTypeSelector.currentIndex() == 1: #x_ds on x-axis; z_ds on y-axis
             if self.TraceYValueChanged:
@@ -452,12 +431,11 @@ def _display_2D_data(self,graphicsView):
                 self.TraceYSelector.setValue(self.TraceYNum)
                 self.TraceYValueChanged = False
 
-            x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'x_ds_url'))
-            y_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'z_ds_url'))       
-            data = data[:,self.TraceYNum,:]
+            dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['x_ds_url', 'z_ds_url'])       
+            data = dss[2][()][:,self.TraceYNum,:]
             
-            fill_x = self.ds.shape[0]
-            fill_y = self.ds.shape[2]
+            fill_x = dss[2].shape[0]
+            fill_y = dss[2].shape[2]
             
         if self.PlotTypeSelector.currentIndex() == 2: #x_ds on x-axis; y_ds on y-axis
             if self.TraceZValueChanged:
@@ -467,12 +445,11 @@ def _display_2D_data(self,graphicsView):
                 self.TraceZSelector.setValue(self.TraceZNum)
                 self.TraceZValueChanged = False
 
-            x_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'x_ds_url'))
-            y_axis_ds = _get_ds(self.ds, _get_ds_url(self.ds, 'y_ds_url'))        
-            data = data[:,:,self.TraceZNum]
+            dss, names, units, scales = _get_all_ds_names_units_scales(self.ds, ['x_ds_url', 'y_ds_url'])       
+            data = dss[2][()][:,:,self.TraceZNum]
             
-            fill_x = self.ds.shape[0]
-            fill_y = self.ds.shape[1]
+            fill_x = dss[2].shape[0]
+            fill_y = dss[2].shape[1]
 
         self.TraceXValue.setText(self._getXValueFromTraceNum(self.ds,self.TraceXNum))
         self.TraceYValue.setText(self._getYValueFromTraceNum(self.ds,self.TraceYNum))
@@ -506,43 +483,24 @@ def _display_2D_data(self,graphicsView):
         data = data / np.nanmean(data,axis=0,keepdims=True)
         np.seterr(**old_warn)
 
-    graphicsView.clear()
-    
-    # pos is the zero-point of the axis  
-      
-    x_axis_scale = _get_axis_scale(x_axis_ds) #entries: (x0, dx)
-    y_axis_scale = _get_axis_scale(y_axis_ds)
-
-    xmin = x_axis_scale[0]-x_axis_scale[1]/2. #center the data around the labels
-    xmax = xmin+fill_x * x_axis_scale[1]
-    ymin = y_axis_scale[0]-y_axis_scale[1]/2.
-    ymax = ymin+fill_y * x_axis_scale[1]
-    
-    pos = (xmin,ymin)
-    
-    # scale is responsible for the "accidential" correct display of the axis
-    # for downsweeps scale has negative values and extends the axis from the min values into the correct direction
-    scale=(x_axis_scale[1],y_axis_scale[1])
-
-    name = _get_name(self.ds)
-    x_axis_name = _get_name(x_axis_ds)
-    x_axis_unit = _get_unit(x_axis_ds)
-    y_axis_name = _get_name(y_axis_ds)
-    y_axis_unit = _get_unit(y_axis_ds)
-    graphicsView.view.setLabel('left', y_axis_name, units=y_axis_unit)
-    graphicsView.view.setLabel('bottom', x_axis_name, units=x_axis_unit)
-    graphicsView.view.setTitle(str(name)+" ("+str(self.unit)+")")
+    graphicsView.clear()   
+    graphicsView.view.setLabel('left', names[1], units=units[1])
+    graphicsView.view.setLabel('bottom', names[0], units=units[0])
+    graphicsView.view.setTitle(names[2]+" ("+units[2]+")")
     graphicsView.view.invertY(False)
     
-    graphicsView.setImage(data,pos=pos,scale=scale)
+    # pos is the zero-point of the axis  
+    # scale is responsible for the "accidential" correct display of the axis
+    # for downsweeps scale has negative values and extends the axis from the min values into the correct direction
+    graphicsView.setImage(data,pos=(scales[0][0]-scales[0][1]/2.,scales[1][0]-scales[1][1]/2.),scale=(scales[0][1], scales[1][1]))
     graphicsView.show()
 
     # Fixme roi ...
     """
     The error in the ROI may come from a not always correct setting of xmin/xmax...
     """
-    graphicsView.roi.setPos([xmin,ymin])
-    graphicsView.roi.setSize([xmax-xmin,ymax-ymin])
+    graphicsView.roi.setPos([scales[0][0],scales[1][0]])
+    graphicsView.roi.setSize([fill_x * scales[0][1],fill_y * scales[1][1]])
     graphicsView.roi.setAcceptedMouseButtons(Qt.RightButton)
     graphicsView.roi.sigClicked.connect(lambda: self.clickRoi(graphicsView.roi.pos(), graphicsView.roi.size()))
     
@@ -562,21 +520,21 @@ def _display_2D_data(self,graphicsView):
                         #Check this for < or <=
                         #Also the x0s and dxs
                         
-                        xval = x_axis_scale[0]+x_index * x_axis_scale[1]
-                        yval = y_axis_scale[0]+y_index * y_axis_scale[1]
+                        xval = scales[0][0]+x_index * scales[0][1]
+                        yval = scales[1][0]+y_index * scales[1][1]
                         zval = data[x_index][y_index]
-                        self.PointX.setText("X: %.6e %s"%(xval,x_axis_unit)) 
-                        self.PointY.setText("Y: %.6e %s"%(yval,y_axis_unit)) 
-                        self.PointZ.setText("Z: %.6e %s"%(zval,self.unit)) 
+                        self.PointX.setText("X: %.6e %s"%(xval,units[0])) 
+                        self.PointY.setText("Y: %.6e %s"%(yval,units[1])) 
+                        self.PointZ.setText("Z: %.6e %s"%(zval,units[2])) 
                         self.data_coord=  "%g\t%g\t%g" % (xval, yval,zval)
 
         else:
             xval = 0
             yval = 0
             zval = 0
-            self.PointX.setText("X: %.6e %s"%(xval,x_axis_unit)) 
-            self.PointY.setText("Y: %.6e %s"%(yval,y_axis_unit)) 
-            self.PointZ.setText("Z: %.6e %s"%(zval,self.unit)) 
+            self.PointX.setText("X: %.6e %s"%(xval,units[0])) 
+            self.PointY.setText("Y: %.6e %s"%(yval,units[1])) 
+            self.PointZ.setText("Z: %.6e %s"%(zval,units[2])) 
             self.data_coord=  "%g\t%g\t%g" % (xval, yval,zval)
     
     self.proxy = pg.SignalProxy(imVi.scene().sigMouseMoved, rateLimit=15, slot=mouseMoved)
