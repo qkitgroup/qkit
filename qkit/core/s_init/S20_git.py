8a6195bf5bf6e59b00b835d233bc76684caff00e
Schneider1 <andre.schneider@kit.edu>
2018-05-09 11:12:05 +0200

    S20_git: bugfix, now always dies silently and detects timestamp more robustly

--- qkit/core/s_init/S20_git.py
+++ qkit/core/s_init/S20_git.py
@@ -14,12 +14,12 @@ try:
         f.seek(-1024, 2)
         last_commit = f.readlines()[-1].split("\t")[0]  # The line looks like this: 0old_commit 1new_commit 2username 3mail 4timestamp 5timezone
         qkit.git = {
-            'timestamp': float(last_commit.split(" ")[4]),
+            'timestamp': float(last_commit.split(" ")[-2]),
             'commit_id': last_commit.split(" ")[1]
         }
         if (time() - qkit.git['timestamp']) / 3600 / 24 > 21:  # If the last commit is older than 3 weeks
             logging.warning("Your qkit version is older than 3 weeks. Please consider the 'git pull' command. We are usually trying to get better.")
-except IOError:
+except Exception:
     qkit.git = {
         'timestamp': None,
         'commit_id': "UNTRACKED-NO_GIT_FOUND"
