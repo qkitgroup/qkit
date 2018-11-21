3a6b97050e92eaa220d1969958eb099b750eda66
TIIIMMMW <tim.wolz@kit.edu>
2018-11-21 14:17:34 +0100

    install.bat: all necessary packages for juypterlab support can be installed automatically

--- install.bat
+++ install.bat
@@ -23,13 +23,64 @@ if not errorlevel 1 (
 	)
 )
 @pause
+@echo.
+@echo.
+@echo.
+@echo off
+Setlocal EnableDelayedExpansion
+
+@echo Do you want to install pyqtgraph, absolutely necessary for plots (y/n)?
+SET /P install=
+if !install!==y (
+    pip install pyqtgraph
+    )
 
 @echo.
 @echo.
+@echo Do you want to install zerorpc, used for device communication (y/n)?
+SET /P install=
+if !install!==y (
+    pip install zerorpc
+    )
 @echo.
-@echo The next thing is to install pyqtgraph module if not already installed.
 @echo.
-@pause
-pip install pyqtgraph
-
+@echo Do you want to install jupyter lab (y/n)?
+SET /P install=
+if !install!==y (
+    pip install jupyterlab==0.33.12
+    )
+@echo.
+@echo.
+@echo Do you want to install ipywidgets, used for nice feature in the notebooks (y/n)?
+SET /P install=
+if !install!==y (
+    echo we need nodejs. If conda is not installed you must install it manually before you continue.
+    echo Continue using conda [y/n]?
+    SET /P conda=
+    if !conda!==y (
+        conda install -c conda-forge nodejs=8.9.3
+    ) else (
+        echo Please install it manually before continuing.
+        pause
+        )
+    pip install ipywidgets
+    jupyter nbextension enable --py widgetsnbextension
+    jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.36
+    )
+@echo.
+@echo.
+@echo Do you want to install qgrid, used for a nice overview of your measurement database (y/n)?
+SET /P install=
+if !install!==y (
+    pip install qgrid
+    jupyter nbextension enable --py --sys-prefix qgrid
+    jupyter labextension install qgrid
+    )
+@echo.
+@echo.
+@echo Do you want to install dill, a pickle extension, that lets you save and load your virtual awg channels (y/n)?
+SET /P install=
+if !install!==y (
+    pip install dill
+    )
 @pause
