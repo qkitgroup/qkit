transport.py: bugfix catch errors in numerical derivative

qkit/measure/transport/transport.py
@@ -284,7 +284,11 @@ class transport(object):
             An N-dimensional array containing numerical derivative dy/dx.
         """
         ### TODO: care about +/-np.inf and viewer
-        return self._numder_func(y, *self._numder_args, **self._numder_kwargs) / self._numder_func(x, *self._numder_args, **self._numder_kwargs)
+        ### TODO: catch error, if len(dataset) < window_length in case of SavGol filter
+        try:
+            return self._numder_func(y, *self._numder_args, **self._numder_kwargs) / self._numder_func(x, *self._numder_args, **self._numder_kwargs)
+        except:
+            return np.zeros(len(y))*np.nan
     
     def set_x_dt(self, x_dt):
         """