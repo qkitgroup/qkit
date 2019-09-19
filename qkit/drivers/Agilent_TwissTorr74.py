931f17419b41c038ebcf0f842793181e29200654
yschoen <y.schoen@kit.edu>
2019-09-19 14:04:04 +0200

    Agilent_TwissTorr74: Finished rewrite V1

--- qkit/drivers/Agilent_TwissTorr74.py
+++ qkit/drivers/Agilent_TwissTorr74.py
@@ -1,183 +1,81 @@
-# Agilent Twisstorr74 class, to perform the communication between the Wrapper and the device
+# Agilent_TwissTorr74 version 1.0 written by YS@KIT 2019
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 import qkit
 from qkit.core.instrument_base import Instrument
 import logging
-import time
 import serial
-import struct
-import time,sys
+import time
 import atexit
-import numpy as np
 
 class Agilent_TwissTorr74(Instrument):
 
     def __init__(self, name, address, reset=False):
         Instrument.__init__(self, name, tags=['physical'])
-            
         logging.info(__name__ + ': Initializing instrument Twisstorr')
         self.setup(address)
-    
+
     def setup(self, device="COM4"):
-        # open serial port, 9600, 8,N,1, timeout 1s
+        # Open serial port, 9600, 8,N,1, timeout 1s
         baudrate = 9600
         timeout = 0.1
 
         # Serial port configuration
-        ser=serial.Serial()
-        ser.baudrate=baudrate
-        ser.timeout=timeout
-        self.ser = self._std_open(device,baudrate,timeout)
+        ser = serial.Serial()
+        ser.baudrate = baudrate
+        ser.timeout = timeout
+        self.ser = self._std_open(device, baudrate, timeout)
         atexit.register(self.ser.close)
-
-        #ser.port='COM4' # com4
-        # load inficon comands
-        #self.cmds = self.twisstorr_cmds()
-
-    def close(self):
-        # open serial port, 9600, 8,N,1, timeout 1s
-        self.ser.close()
-        
-    def _set_remote_serial(self):
-        # start
-        remote_to_serial= [0x02,0x80,0x30,0x30,0x38,0x31,0x30,0x03,0x42,0x41]
-        comand=self.ret_output(remote_to_serial)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return value_hex    
-
-    def _set_speed_reading_after_stop(self):
-        # start
-        reading_speed_after_stop= [0x02,0x80,0x31,0x36,0x37,0x31,0x30,0x03,0x42,0x32]
-        comand=self.ret_output(reading_speed_after_stop)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return value_hex      
-
-    def _active_stop(self):
-        # active stop
-        active_stop= [0x02,0x80,0x31,0x30,0x37,0x31,0x31,0x03,0x42,0x35]
-        comand=self.ret_output(active_stop)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return value_hex      
-
-    def set_start(self):
-        # start
-        start= [0x02,0x80,0x30,0x30,0x30,0x31,0x31,0x03,0x42,0x33]
-        comand=self.ret_output(start)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return value_hex    
-    
-    def set_stop(self):
-        # stop
-        stop= [0x02,0x80,0x30,0x30,0x30,0x31,0x30,0x03,0x42,0x32]
-        comand=self.ret_output(stop)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return value_hex
-
-    def get_temperature(self):
-        # get temperature
-        temperature=[0x02,0x80,0x32,0x30,0x34,0x30,0x03,0x38,0x35]
-        comand=self.ret_output(temperature)
-        
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        if value_hex[0:5]==(2, 128, 50, 48, 52):
-            Temperature=float(''.join(value_chr[5:12]))
-        else:
-            print('response from Turbo not in window 205 on RS 235')
-            print(value_hex)
-        return 'Pump Temperature: '+ str(Temperature)
-
-    def get_pumpstatus(self):
-        # get pump status
-        status = [0x02,0x80,0x32,0x30,0x35,0x30,0x03,0x38,0x34]
-        comand=self.ret_output(status)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        Pumpstatus ={
-                  0: "Stop",
-                  1: "Waiting initlk",
-                  2: "Starting",
-                  3: "Auto-tuning",
-                  4: "Braking",
-                  5: "Normal",
-                  6: "Fail"
-                }
-        return 'Pump status is: '+str(Pumpstatus[int(''.join(value_chr[5:12]))])
-
-    def get_rotationspeed(self):
-        # get rotationspeed
-        rotationspeed=[0x02,0x80,0x32,0x32,0x36,0x30,0x03,0x38,0x35]
-        comand=self.ret_output(rotationspeed)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return 'The rotationspeed of the pump is: '+str(float(''.join(value_chr[5:12]))/60.0)+' Hz'
-
-    def get_rotationspeed_setting(self):
-        # get rotationspeed
-        rotationspeed=[0x02,0x80,0x31,0x32,0x30,0x30,0x03,0x38,0x30]
-        comand=self.ret_output(rotationspeed)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return 'The rotationspeed of the pump is: '+str(float(''.join(value_chr[5:12])))+' Hz'
-
-    def set_rotationspeed1100_setting(self):
-        # set rotation frequency to 1100 Herz, does not work at the moment
-        rotationspeed=[0x80,0x31,0x32,0x30,0x31,0x30,0x30,0x31,0x31,0x30,0x30,0x03,0x38,0x31]
-        comand=self.ret_output(rotationspeed)
-        value_bin=self.remote_cmd(comand)
-        value_hex=self.ret_value(value_bin)
-        value_chr=map(lambda x: chr(value_hex[x]),range(len(value_hex)))
-        return ''.join(value_chr[5:12])
+        self._set_remote_serial(serial=True)
+        if self.get_status == 0:
+            self._set_active_stop(active_stop=True)
 
     def _std_open(self, device, baudrate, timeout):
         return serial.Serial(device, baudrate, timeout=timeout)
 
-    def remote_cmd(self, cmd):
-        self.ser.write(cmd)      
+    def close(self):
+        # Close serial port
+        self.ser.close()
+
+    def _remote_cmd(self, cmd):
+        """
+        Function sending the command string to the device.
+
+        Args:
+            cmd (str): Command string to be sent. Function "command" can be used to build it
+                       from supplied window and data information (see pump manual for possible values)
+
+        Returns:
+            value (str): Answer from the device. Function "answer" can be use to unpack useful info.
+        """
+        self.ser.write(cmd)
         time.sleep(0.1)
         rem_char = self.ser.inWaiting()
-        
         value = self.ser.read(rem_char)
-        #print "##"+value+"##"+value.strip()+"###"
-        return value #value.strip()
-    
-    def ret_output(self,data):
-        N=np.size(data)
-        format = ">"
-        for i in range(N):
-            format = format + "B" # 
-        return struct.pack(format, *data)#
+        return value
 
-    def ret_value(self,data):
-        N=len(data)
-        format = ">"
-        for i in range(N):
-            format = format + "B" # 
-        return struct.unpack(format, data)
-
-    def command(self, addr=0x80, window, rw, data=None):
+    def command(self, addr=0x80, window='205', write=False, data=None):
         """
         Function building the command string sent to the device.
 
         Args:
             addr (hex): Unit address = 0x80 for RS-232, 0x80 + device number for RS-485
             window (str): String of 3 numeric character indicating the window number (defining the command)
-            rw (str): Is the command used to read ('0') or write ('1') to the device
+            write (bool): Is the command used to read (false) or write (True) to the device
             data (str):  Alphanumeric ASCII string with the data to be written in case rw=1
 
         Returns:
@@ -186,15 +84,18 @@ class Agilent_TwissTorr74(Instrument):
 
         start = '\x02'
 
-        command_string = start + chr(addr) + window + rw
-        if rw == 1:
+        command_string = start + chr(addr) + window
+        if write == True:
+            command_string += '1'
             command_string += data
+        else:
+            command_string += '0'
         end = '\x03'
         command_string += end
 
-        crc = ord(command_string[1])^ord(command_string[2])
-        for i in range(3,len(command_string)):
-            crc = crc^ord(command_string[i])
+        crc = ord(command_string[1]) ^ ord(command_string[2])
+        for i in range(3, len(command_string)):
+            crc = crc ^ ord(command_string[i])
         crch = hex(crc)
         crc1 = crch[-2]
         crc2 = crch[-1]
@@ -204,51 +105,116 @@ class Agilent_TwissTorr74(Instrument):
         return cmd
 
     def answer(self, answer):
+        """
+        Function unpacking the device answer.
+        If the returned string has the length corresponding to one char of information,
+        this information is decoded using the described meanings from the manual.
+        In the case that the returned string is longer,
+        the characters containing the data are returned.
+        """
         start = answer[0]
         address = answer[1]
-        if answer[2] == chr(0x06):
-            return answer[2]
+        if len(answer) == 6:
+            if answer[2] == chr(0x06):
+                return answer[2], 'ACK'
+            if answer[2] == chr(0x15):
+                return answer[2], 'NACK'
+            if answer[2] == chr(0x32):
+                return answer[2], 'Unknown Window'
+            if answer[2] == chr(0x33):
+                return answer[2], 'Data Type Error'
+            if answer[2] == chr(0x34):
+                return answer[2], 'Out of Range'
+            if answer[2] == chr(0x35):
+                return answer[2], 'Window Read Only or Disabled'
+            else:
+                return answer
         else:
             window = answer[2] + answer[3] + answer[4]
             rw = answer[5]
-            for i in range(6,len(answer)-3):
+            data = ''
+            for i in range(6, len(answer) - 3):
                 data += answer[i]
             return data
 
-    def start(self):
-        window = '000'
-        rw = '1'
-        data = '1'
-        cmd = self.command(window=window, rw=rw, data=data)
-        self.remote_cmd(cmd)
+    def _set_remote_serial(self, serial=True):
+        window = "008"
+        write = True
+        if serial:
+            data = '0'
+        else:
+            data = '1'
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
 
-    def stop(self):
-        window = "000"
-        rw = '1'
-        data = '0'
-        cmd = self.command(window=window, rw=rw, data=data)
-        self.remote_cmd(cmd)
+    def _get_remote_serial(self):
+        window = "008"
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
 
-    def set_softstart(self, status=False):
-        window = "100"
-        rw = '1'
-        if status: data = "1"
-        else: data = "0"
-        cmd = self.command(window=window, rw=rw, data=data)
-        self.remote_cmd(cmd)
+    def _set_active_stop(self, active_stop=False):
+        window = "107"
+        write = True
+        if active_stop:
+            data = '1'
+        else:
+            data = '0'
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
 
-    def set_rot_freq_setting(self, freq=1167):
+    def _get_active_stop(self):
+        window = "107"
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def get_status(self):
         """
-        Rotational frequency setting (Hz)
+        Get the pump status.
 
-        Args:
-            freq (int): Rotational frequency [1100..1167], default = 1167
+        Returns:
+            status (int):
+                0: 'Stop'
+                1: 'Waiting Interlock'
+                2: 'Starting'
+                3: 'Auto-tuning'
+                4: 'Breaking'
+                5: 'Normal'
+                6: 'Fail'
         """
-        window = "000"
-        rw = '1'
-        data = str(freq)
-        cmd = self.command(window=window, rw=rw, data=data)
-        self.remote_cmd(cmd)
+        window = '205'
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        meaning = ['Stop', 'Waiting Interlock', 'Starting', 'Auto-tuning', 'Braking', 'Normal', 'Fail']
+        return int(answer), meaning[int(answer)]
+
+    def get_error(self):
+        """
+        Get error code.
+
+        Returns:
+            Error code (Bitstring).
+            Bit description as taken from manual: |7|6|5|4|3|2|1|0|
+                7: Too high load
+                6: Short circuit
+                5: Over voltage
+                4:
+                3:
+                2: Control over temperature
+                1: Pump over temperature
+                0: No connection
+        """
+        window = "206"
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
 
     def get_rot_freq(self):
         """
@@ -258,8 +224,145 @@ class Agilent_TwissTorr74(Instrument):
             freq (int): Rotation frequency in Hz
         """
         window = "226"
-        rw = '0'
-        cmd = self.command(window=window, rw=rw)
-        answer = self.answer(self.remote_cmd(cmd))
-        answer = int(answer)/60.
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        answer = int(answer) / 60.
         return answer
+
+    def get_temp(self):
+        """
+        Get pump temperature
+
+        Returns:
+            temp (int): Pump temperature in degC
+        """
+        window = '204'
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def get_power(self):
+        """
+        Get pump power
+
+        Returns:
+            power (int): Pump power in Watts
+        """
+        window = '202'
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def get_cycles(self):
+        """
+        Get pump cycle number
+
+        Returns:
+            cycle (int): Cycle number
+        """
+        window = '301'
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def get_pump_life(self):
+        """
+        Get pump life time
+
+        Returns:
+            pump life (int): Pump life in hours
+        """
+        window = '302'
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def start(self):
+        """
+        Start the pump
+        """
+        window = '000'
+        write = True
+        data = '1'
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
+
+    def stop(self):
+        """
+        Stop the pump
+        """
+        window = "000"
+        write = True
+        data = '0'
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
+
+    def set_softstart(self, status=False):
+        """
+        Set soft start option.
+        Makes the pump start slower. Manual recommends after longer standing times.
+        See manual.
+
+        Args:
+            status (bool): Status of soft start option to be set (standard: False)
+        """
+        window = "100"
+        write = True
+        if status:
+            data = "1"
+        else:
+            data = "0"
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
+
+    def get_softstart(self):
+        """
+        Get status of soft start option.
+        Makes the pump start slower. Manual recommends after longer standing times.
+        See manual.
+
+        Returns:
+            status (bool): Status of soft start option
+        """
+        window = "100"
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
+
+    def set_rot_freq_setting(self, freq=1167):
+        """
+        Rotational frequency setting (Hz)
+
+        Args:
+            freq (int): Rotational frequency [1100..1167], default = 1167
+        """
+        window = "120"
+        write = True
+        data = str(freq)
+        while len(data) < 6:
+            data = '0' + data
+        cmd = self.command(window=window, write=write, data=data)
+        answer = self.answer(self._remote_cmd(cmd))
+        return answer
+
+    def get_rot_freq_setting(self):
+        """
+        Get rotational frequency setting (Hz)
+
+        Returns:
+            freq (int): Rotational frequency [1100..1167], default = 1167
+        """
+        window = "120"
+        write = False
+        cmd = self.command(window=window, write=write)
+        answer = self.answer(self._remote_cmd(cmd))
+        return int(answer)
