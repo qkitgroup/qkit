20bbb032de7a25b919a69ff5cdc6ac9f4f25fee3
AngryRabbitChicken <julian.ferrero@kit.edu>
2021-04-21 14:12:41 +0200

    Started measurement file, HEAVY WIPgit status!

---
+++ qkit/measure/spin_tune/spin_tune.py
@@ -0,0 +1,63 @@
+# spin_tune.py intented for use with a voltage source and an arbitrary I-V-device or lockin
+# JF@KIT 04/2021
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+################################################################## Julian Added
+import qkit
+import qkit.measure.measurement_base as mb
+from qkit.gui.notebook.Progress_Bar import Progress_Bar
+################################################################## Old
+
+import numpy as np
+
+
+##################################################################
+class IV_meas(mb.MeasureBase):
+    def __init__(self, exp_name = "", sample = None):
+        mb.MeasureBase.__init__(self, sample)
+        
+        self._get_value_func = None
+        self._get_tracedata_func = None
+        
+    def set_get_value_func(self, get_func, *args, **kwargs):
+        if not callable(get_func):
+            raise TypeError("%s: Cannot set %s as get_value_func. Callable object needed." % (__name__, get_func))
+        self._get_value_func = lambda: get_func(*args, **kwargs)
+        self._get_tracedata_func = None
+        
+    def set_get_tracedata_func(self, get_func, *args, **kwargs):
+        if not callable(get_func):
+            raise TypeError("%s: Cannot set %s as get_tracedata_func. Callable object needed." % (__name__, get_func))
+        self._get_tracedata_func = lambda: get_func(*args, **kwargs)
+        self._get_value_func = None
+        
+    def measure1D(self):
+        #add useful information about the measurement
+        self._measurement_object_measurement_func = "measure1D_spin_tune"
+        
+        pb = Progress_Bar(len(self._x_parameter.values))
+        
+        def create_file()
+        #implement creation of save file right here?
+        for index, x_val in enumerate(self._x_parameter.values):
+            self._x_parameter.set_function(x_val)
+            qkit.flow.sleep(self._x_parameter.wait_time)
+            try:
+                
+                pb.iterate()
+            
+            #or maybe here?
+            #if index == 0:
+            #   create_save()
