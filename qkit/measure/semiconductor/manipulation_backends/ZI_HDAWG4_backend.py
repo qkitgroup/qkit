039c52db4cf10af1f9bcfb9f38e795b93fb3bd72
thomas.koch <thomas.koch4@kit.edu>
2021-09-17 14:59:35 +0200

    sampling rate in HDAWG4 backend fixed

--- qkit/measure/semiconductor/manipulation_backends/ZI_HDAWG4_backend.py
+++ qkit/measure/semiconductor/manipulation_backends/ZI_HDAWG4_backend.py
@@ -24,35 +24,35 @@ class ZI_HDAWG4_backend(MA_backend_base):
         self.wave_data = wave_data
         
     def Ch1_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
     
     def Ch2_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
     
     def Ch3_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
     
     def Ch4_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
     
     def Trig1_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
 
     def Trig2_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
 
     def Trig3_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
 
     def Trig4_get_sample_rate(self):
-        rate = self.hartwig.get_sampling_clock()*1e-9
+        rate = self.hartwig.get_sampling_rate()*1e-9
         return rate
     
     
