83bc6b9bdaf6a1baf1de7deea1ad7e1ab3c1d115
thomas.koch <thomas.koch4@kit.edu>
2021-09-17 11:16:24 +0200

    added chX_get_sample_rate methods

--- qkit/measure/semiconductor/manipulation_backends/ZI_HDAWG4_backend.py
+++ qkit/measure/semiconductor/manipulation_backends/ZI_HDAWG4_backend.py
@@ -9,23 +9,52 @@ from MA_backend_base import MA_backend_base
 
 class ZI_HDAWG4_backend(MA_backend_base):
     def __init__(self, instrument, wave_data):
-        self.register_channel("Ch1")
-        self.register_channel("Ch2")
-        self.register_channel("Ch3")
-        self.register_channel("Ch4")
+        self.register_channel("Ch1", "V")
+        self.register_channel("Ch2", "V")
+        self.register_channel("Ch3", "V")
+        self.register_channel("Ch4", "V")
         
-        self.register_channel("Trig1")
-        self.register_channel("Trig2")
-        self.register_channel("Trig3")
-        self.register_channel("Trig4")
+        self.register_channel("Trig1", "V")
+        self.register_channel("Trig2", "V")
+        self.register_channel("Trig3", "V")
+        self.register_channel("Trig4", "V")
         
         self.hartwig = instrument
         
         self.wave_data = wave_data
-    
-    def get_sample_rate(self):
+        
+    def Ch1_get_sample_rate(self):
         rate = self.hartwig.get_sampling_clock()
         return rate
+    
+    def Ch2_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+    
+    def Ch3_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+    
+    def Ch4_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+    
+    def Trig1_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+
+    def Trig2_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+
+    def Trig3_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+
+    def Trig4_get_sample_rate(self):
+        rate = self.hartwig.get_sampling_clock()
+        return rate
+    
     
     def run(self):
         self.hartwig.start_playback()
@@ -37,4 +66,5 @@ class ZI_HDAWG4_backend(MA_backend_base):
         self.hartwig.zdict_to_CSV(self.wave_data)
         self.hartwig.zcreate_sequence_program(0)
         self.hartwig.upload_to_device()
-    
+        
+        
