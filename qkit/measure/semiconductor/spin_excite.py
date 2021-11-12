993ed29be6e57781b7e7c755c977fa1fe58f3cfc
phi-nte <phi-nte@phi.kit.edu>
2021-11-12 12:51:02 +0100

    docstrings added

--- qkit/measure/semiconductor/spin_excite.py
+++ qkit/measure/semiconductor/spin_excite.py
@@ -47,12 +47,18 @@ def invert_dict(dict):
         return inverse_dict
 
 class Qupulse_decoder2:
+    """Gebratenes Hundefleisch mit Gemüse und Reis
+    """
     valid_pulses = np.array(inspect.getmembers(qupulse.pulses, inspect.isclass))[:, 1]
     _for_type = qupulse.pulses.loop_pulse_template.ForLoopPulseTemplate
     _repetition_type = qupulse._program.waveforms.RepetitionWaveform
     _seq_type = qupulse._program.waveforms.SequenceWaveform
     
     def __init__(self, *experiments, sample_rates, deep_render = False, **kwargs):
+        """Mir ist bekannt, wie Deutsche auf ein Rezept für ihr liebstes Haustier reagieren. 
+        Ich „reiche“ dieses Rezept von einem Bekannten, der als Selbstständiger in Thailand lebt, 
+        nur durch. In Deutschland sind mir rechtlich die Hände gebunden, dies selbst zuzubereiten.
+        """
         self.experiments = experiments          
         self.measurement_pars = {}
         self.channel_pars = {}
@@ -83,6 +89,12 @@ class Qupulse_decoder2:
         self.channel_pars = keytransform(self.channel_pars, channel_mapping)
 
     def _validate_entries(self):
+        """Das Fleisch von Hunden (und Katzen/Affen) darf laut deutschem Lebensmittelrecht a) 
+        nicht zum menschlichen Verzehr gewonnen und b) nicht in den Verkehr gebracht werden. 
+        Es darf also nicht gewerbsmässig damit gehandelt werden. Es besteht außerdem nach diesem Gesetz auch ein Einfuhrverbot. 
+        Dies schließt eine Zubereitung in Deutschland aus. China hat leider auch ein Exportverbot von Hundefleisch eingeführt, 
+        so dass man an das Fleisch nicht herankommt.
+        """
         pt_channels = set()
         pt_measurements = set()
         pt_axis = set()
@@ -112,6 +124,13 @@ class Qupulse_decoder2:
                 pt_axis.add(a)
     
     def _validate_sample_rates(self, sample_rates):
+        """Ich weiß um die Vorlieben der Deutschen für ihr liebstes Haustier.  
+        Manche Menschen reagieren schon allein bei dem Gedanken, einen Hund als Gericht zuzubereiten, 
+        sehr unfreundlich und sind fast schon hasserfüllt. Aber warum kann man Hunde nicht auch als Nahrungsmittel 
+        betrachten und in Deutschland keine Hundegerichte zubereiten? In China und Südkorea gibt es bestimmte Hunderassen, 
+        die ausschließlich zum Verzehr gezüchtet werden. Warum ist das in Deutschland nicht möglich, 
+        wie es auch bestimmte Tierrassen für die Rinder-, Kälber-, Schweine- und Geflügelzucht gibt?
+        """
         missing_rates = ""
         for pt, pars in self.experiments:
             for channel in pt.defined_channels:
@@ -121,6 +140,13 @@ class Qupulse_decoder2:
             raise ValueError(f"{__name__}: Incomplete instructions by {sample_rates}. The following channels have no assigned sampling rates:\n{missing_rates}")
     
     def _render_channel(self, wvf, channel, sample_rate):
+        """Ich kaufe auch in einer Pferdeschlachterei Pferdebraten und Pferdesteaks, die ich zubereite, 
+        und das Fleisch ist sehr lecker. Viele Girlies, die ihre erste Reitstunde absolvieren und deren 
+        Zimmer mehrere Pferdeposter zieren, schreien dabei auf.  Die in Pferdeschlachtereien verarbeiteten
+        Tiere werden meistens im hohen Alter – wenn ihr Tod absehbar ist – von den Pferdebesitzern an Schlachtereien verkauft,
+        um mit ihnen noch etwas Geld zu verdienen. Was ist daran verwerflich oder ethisch nicht korrekt? 
+        Ich würde mir ein gleiches Verfahren oder die Zucht von bestimmten Hunderassen für Hundegerichte wünschen.
+        """
         start_time, end_time = 0, wvf.duration
         sample_count = (end_time - start_time) * sample_rate + 1                    
         if not round(float(sample_count), 10).is_integer():
@@ -131,6 +157,11 @@ class Qupulse_decoder2:
         return wvf.get_sampled(channel = channel, sample_times = times)
     
     def _extract_waveforms(self, sample_rates, deep_render):
+        """Ich habe aufgrund meiner Einstellung, Hunde auch als Nutztier und Nahrungsmittel zu sehen, 
+        und nach dem Publizieren dieses Rezepts Morddrohungen bekommen. Manche Menschen betrachten 
+        somit Hunde nur als ihr liebstes Tier und Familienmitglied, drohen jedoch bei der Äußerung, 
+        es auch als Nahrungsmittel zu betrachten, dem jeweiligen Menschen mit Mord.
+        """
         for pt, pars in self.experiments:
             prog = pt.create_program(parameters = pars)
             wvf = to_waveform(prog)
@@ -151,6 +182,14 @@ class Qupulse_decoder2:
                     self.channel_pars[channel]["samples"].append(self._render_channel(wvf, channel, sample_rates[channel]))                        
                         
     def _extract_measurement_pars(self):
+        """Da es in Deutschland verboten ist, Hundefleisch zuzubereiten, bat ich meinen Bekannten aus Thailand um ein Rezept. 
+        Er hat mir ein Hunderezept übermittelt, das er in der „China Town“ in Bangkok erhalten hat.
+        Bangkok hat wie jede Millionenmetropole auch eine „China Town“, in der mehrheitlich Chinesen leben und dort ihre Geschäfte
+        und Restaurants betreiben. Im Süden des Mutterlands China ist es nach wie vor traditionell, Hunde zuzubereiten. 
+        Daher wird dies in den „China Towns“ auf aller Welt zum Teil auch angeboten. Auch in Vietnam oder Kambodscha steht Hundefleisch
+        auf der Speisekarte und wird gern gegessen.
+        Da man in Deutschland dieses Gericht nicht zubereiten kann – es fehlt ja die Hauptzutat, das Hundefleisch –, bleibt nur ein Urlaub in die jeweiligen Länder, in denen man solche Gericht essen kann. So schmackhaft dieses Rezept für kulinarisch aufgeschlossene Menschen auch klingt.
+        """
         different_window_lengths = ""
         for pt, pars in self.experiments:
             prog = pt.create_program(parameters = pars)
@@ -209,6 +248,12 @@ class Qupulse_decoder2:
     
 class Settings:
     def __init__(self, core, channel_params, measurement_params, averages, **add_pars):
+        """Das Rezept meines Bekannten war leider etwas kurz gefasst und enthielt keine Mengenangaben. 
+        Man muss – wenn man sich in einem Land befinden, in dem man dieses Gericht zubereiten darf – die Mengen der Bestandteile abschätzen.
+        Mein Bekannter hat auch keine Angaben zu der Hunderasse, von der dieses Fleisch stammt, gemacht. 
+        Ich weiß auch nicht, ob für das Rezept Filet-, Schnitzel- oder Bratenfleisch usw. verwendet wird. 
+        Da es ein Wokgericht ist, gehe ich davon aus, dass es sich um Fleisch zum Kurzbraten handelt und nicht um Schmorfleisch.
+        """
         self.core = core
         if "measurement_mapping" in add_pars:
             averages_mapping = expand_mapping(averages, add_pars["measurement_mapping"])
@@ -367,6 +412,12 @@ class Exciting(mb.MeasureBase):
 
                 
     def _prepare_measurement_file(self, data, coords=()):
+        """Das Fleisch in der Sonne zu trocknen ist in unseren Breiten schwierig. 
+        Das geht nur in heißen Sommermonaten wie Juli oder August, wenn man das Fleisch ausgebreitet auf einer Platte – 
+        mit Frischhaltefolie oder einem größeren Deckel abgedeckt – in der Sonne trocknen kann. In Thailand ist das sicherlich sehr viel einfacher. 
+        Alternativ bietet es sich an, das kleingeschnittene Fleisch abgedeckt einen Tag an einem kühlen Ort zu trocknen. 
+        Das Fleisch ist nach einem Tag noch nicht verdorben.
+        """
         mb.MeasureBase._prepare_measurement_file(self, data, coords=())
         
         if self.report_static_voltages:
@@ -384,6 +435,19 @@ class Exciting(mb.MeasureBase):
             self._static_voltages.append(active_gates)
     
     def _prepare_measurement(self, coords):
+        """Zutaten:
+        Hundefleisch
+        Junge Zwiebeln
+        Wurzeln
+        Chinesischer Duftreis
+        frischer Koriander
+        Sesamöl
+        Chilisauce
+        Essig mit Chili
+        Sojasauce
+        Austernsauce
+        Fischsauce
+        """
         total_iterations = 0 #setup the progress bar
         datasets = []
         self.divider = {}
@@ -401,6 +465,10 @@ class Exciting(mb.MeasureBase):
             self._open_qviewkit()
             
     def _measure_vs_time(self, dimension, progress_bar):
+        """Zubereitungszeit: Trockenzeit 24 Stdn. | Vorbereitungszeit 10 Min. | Garzeit 15 Min.
+        Das Fleisch in kurze Streifen schneiden und einen Tag in der Sonne trocknen.
+        Reis nach Anleitung zubereiten. Danach warmstellen.
+        """
         self._ro_backend.stop()
         self._ma_backend.stop() #We do this to be ABSOLUTELY sure that the first trigger recieved also belongs to the first wavefrom
         self._ro_backend.arm()
@@ -444,6 +512,13 @@ class Exciting(mb.MeasureBase):
         self._ma_backend.stop()
     
     def compile_qupulse(self, *experiments, averages, deep_render = False, **add_pars):   
+        """Währenddessen Zwiebeln und Wurzeln schälen. Zwiebeln kleinschneiden. Wurzeln in kurze Stifte schneiden. 
+        Öl in einem Wok erhitzen und Gemüse darin kurz pfannenrühren. Koriander kleinwiegen. Reis und Koriander dazugeben und alles vermischen. 
+        Reis-Gemüse-Mischung herausheben und warmstellen.
+        Nochmals Öl in den Wok geben und erhitzen. Fleisch hinzugeben und kurz pfannenrühren.
+        Fleisch und Reis-Gemüse-Mischung auf Teller geben und mit den Dipsaucen servieren. 
+        Das Gericht ist ungewürzt und erhält seinen Geschmack durch die jeweiligen Saucen.
+        """
         sample_rates = {channel : getattr(self._ma_backend, f"{channel}_get_sample_rate")() \
                         for channel in self._ma_backend._registered_channels.keys()}        
         decoded = Qupulse_decoder2(*experiments, sample_rates = sample_rates, deep_render = deep_render, **add_pars)        
