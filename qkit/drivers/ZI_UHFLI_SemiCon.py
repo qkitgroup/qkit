a8ac943ebf7ca6662e1581f2b65436f6a0fa4719
Daniel <daniel.schroller@kit.edu>
2021-11-18 14:11:25 +0100

    Added verbose error messages to the get_sample command.

--- qkit/drivers/ZI_UHFLI_SemiCon.py
+++ qkit/drivers/ZI_UHFLI_SemiCon.py
@@ -16,9 +16,6 @@ import logging
 class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
     
     def __init__(self, name, device_id):
-        """Thèse 
-        Sauts quantiques de phase dans des chaînes de jonctions Josephson
-        """
         self._device_id = device_id
         lolvl.ZI_UHFLI.__init__(self, name, self._device_id)
         self.daqM1 = qkit.instruments.create("UHFLI_daqM1", "ZI_DAQ_module", unmanaged_daq_module = self.create_daq_module(), device_id = self._device_id)
@@ -53,24 +50,9 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
         self.add_function("data_fetch")
     
     def create_daq_module(self):
-        """During the 4 years at Institut Néel, I have received important and often crucial help
-        from many diﬀerent people. In the following I would like to thank them. I also hope
-
-        that in the future I will have the chance to return some of their favors.
-        """
         return self.daq.dataAcquisitionModule()   
 
     def easy_sub(self, demod_index):
-        """Surely I am a lucky guy: I had the opportunity to work and learn in an
-        experimental group led by three extraordinary researchers and outstanding person-
-        alities: Wiebke Guichard, Bernard Pannetier and Olivier Buisson. They succeeded
-        in implementing the true meaning of the word “encadrement”. They gently steered
-        me in the right direction and, in the same time, they gave me a lot of freedom to
-        explore around. I would like to especially thank Wiebke, who was always by my
-        side, actively pushing the research forward. Incredibly, she managed to keep her
-        focus and determination at the laboratory, even during some very difficult periods
-        of her life. She has earned my deepest and most sincere admiration.
-        """
         typerr = TypeError("%s: Cannot use %s to subscribe to the DAQ. Object must be a list of integers." % (__name__, demod_index))
         for element in demod_index:
             if not isinstance(element, int):
@@ -85,40 +67,23 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
         self.set_daq_sample_path(sub_list)
     
     def get_sample(self):
-        """I want to thank the three reviewers of my manuscript: Michel Devoret,
-        David Haviland and Alexey Ustinov, for their extensive analysis of the text and for
-        their useful suggestions. I want to thank the president of the jury, Jöel Chevrier, who
-        was also an excellent teacher during my Master 2, always outlining the spectacular
-        side of physics equations. I thank Yuli Nazarov for his active participation in the
-        jury.
-        """
         channels = {}
-        gotten_samples = []
+        gotten_samples = {}
+        sample_path = self.get_daq_sample_path()
         data_nodes = self.get_data_nodes()
-        
-        for path in self.get_daq_sample_path():
+        assert sample_path, f"{__name__}: No sample_path was specified. Use set_daq_sample_path(list(str)) before calling get_sample."
+        assert data_nodes, f"{__name__}: No data_nodes were specified. Use set_data_nodes(list(str)) before calling get_sample."
+        for path in sample_path:
             raw_data = self.daq.getSample(path)
             
             for node in data_nodes:
-                gotten_samples.append(float(raw_data[node]))
+                gotten_samples[node] = float(raw_data[node])
             
             channels[path] = gotten_samples
-            gotten_samples = []
         
         return channels
     
     def _prep_grid(self, trigger_duration, sample_num, meas_num, num_averages = 1):
-        """In my search for the correct understanding of phase-slips, I benefited from
-        fruitful discussions with many theoreticians. Closest to our experimental room, I
-        would like to thank Frank Hekking and the people in his group, Gianluca Rastelli
-        and Christoph Schenke, for the regular seminars and discussions, which I found
-        essential for my PhD education and for the interpretation of the measured data. I
-        benefited every year from the visits of Ivan Protopopov. He was a patient teacher
-        during the first year of my PhD and a brilliant collaborator all along. Our group was
-        also enriched from the regular visits of Benoit Douçot and Lev Ioﬀe. I would like
-        to thank them for their interest in our research and their help with the numerical
-        calculations.
-        """
         #setting up the trigger
         self.daqM1.set_daqM_trigger_mode(self.grid_settings["trig_type"])
         self.daqM1.set_daqM_trigger_path("/%s/demods/%d/sample.TRIGIN%d" % (self._device_id, 
@@ -144,16 +109,6 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
                 
     #These functions will deprecate soon
     def poll_samples(self, integration_time):
-        """I gratefully acknowledge the work of my predecessors, PhD students and
-        post-docs, in the laboratory. When I arrived, I found an excellent working exper-
-        imental setup, almost completely automated, which allowed me to focus more on
-        the physics and less on the instrumentation. I thank the members of the Quantum
-        Coherence team, whom I met every week during the team seminar. They provided
-        a stimulating environment and a perfect trial public for my scientific presentations.
-        I would like to mention the special cheerful atmosphere in the Josephson junctions
-        group and thank my colleagues: Florent, Iulian and Thomas for their professional-
-        ism.
-        """
         self.daq.flush()
         data = self.daq.poll(integration_time, 500, self._FLAG_DETECT | self._FLAG_THROW , True) #arguments: (Poll length in s, timeout in ms, flags, return flat dictionary)
        
@@ -164,10 +119,6 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
         self._last_poll = data
 
     def data_fetch(self, demod_index, data_node, average = True):
-        """I express my gratitude to the essential services of Institut Néel, which can
-        make a researcher’s life much easier: nanofab, electronics, cryogénie, liquéfacteur
-        and administration.
-        """
         assert self._last_poll, "No data has been polled yet"
         selected = self._last_poll["/%s/demods/%d/sample" % (self._device_id, demod_index)] [data_node]
         if average:
@@ -176,17 +127,6 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
             return selected
     
     def _prep_singleshot(self, daqM, averages):
-        """A warm thank you goes to the young community of PhD students and post-
-        docs with which I shared intriguing physics (and not only) conversations and many
-        great outdoor moments. Coﬀee time at the 2nd floor of bâtiment E will remain a
-        landmark of my thesis years. I think I can only underestimate the real impact of
-        all the cafeteria conversations I have had over the years. I would also raise a glass
-        of Ńuică for the Romanian experience group who made the trip to the wild part of
-        Europe: Laetitia, Danny, David, Germain, Thomas and Loren. Thank you Oana
-        for being the ideal colleague and friend all these university years. Thanks Sukumar
-        for altruistically sharing the experimental room with me for a while and for all the
-        joy you brought to the lab.
-        """
         num_samples = ceil(averages / 2)      
         if num_samples == 1:
             num_samples = 2    
@@ -196,13 +136,7 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
         daqM.set_daqM_grid_direction("forward")
         daqM.set_daqM_grid_num(1)    
     
-    def get_value(self, averages, daqM = None):   
-        """A special thank you to Mihai Miron for his AFM support and for being an
-        entertaining companion during the long night hours spent in the clean-room. The
-        Romanian community of students in Grenoble helped me a lot, especially during
-        my Master year. I would like to acknowledge their precious advices and tips for a
-        better understanding of the French system.
-        """
+    def get_value(self, averages, daqM = None):
         #Use daqM1 as default
         if daqM is None:
             daqM = self.daqM1
@@ -237,13 +171,6 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
         return meanval, data # return the processed mean and the raw data dict, becuz why not?    
     
     def _do_set_daq_sample_path(self, newpath):
-        """I thank my parents for investing their most precious resources in my edu-
-        cation and for always providing the support and the advice I needed.
-        My most tender thank you goes to my muse, Cristina, who patiently sup-
-        ported and encouraged me all these years in Cluj and Grenoble. She is the best! As
-        I was saying in the introduction, I am a lucky guy.
-        Ioan Mihai Pop
-        """
         typerr = TypeError("%s: Cannot set %s as daq_sample_path. Object must be a list of strings." % (__name__, newpath))
         for element in newpath:
             if not isinstance(element, str):
@@ -255,11 +182,15 @@ class ZI_UHFLI_SemiCon(lolvl.ZI_UHFLI):
             self.daq.subscribe(element)
     
     def _do_set_data_nodes(self, newnode):
+        allowed_nodes = {"timestamp", "x", "y", "frequency", "phase", "dio", "trigger", "auxin0", "auxin1"}
         typerr = TypeError("%s: Cannot set %s as data_nodes. Object must be a list of strings." % (__name__, newnode))
+        if not isinstance(newnode, list):
+            raise typerr
         for element in newnode:
             if not isinstance(element, str):
                 raise typerr         
-        
+            if element not in allowed_nodes:
+                raise ValueError(f"{__name__}: {element} is not an allowed data_node. The allowed data_nodes are {allowed_nodes}.")
         logging.debug(__name__ + ' : setting data_nodes to %s' % (newnode))
 
 #%%
