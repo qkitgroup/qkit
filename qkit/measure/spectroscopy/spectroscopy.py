52a5204f2bc41e8477635dc5e26f8cf74958008e
MichaWildermuth <micha.wildermuth@kit.edu>
2022-02-25 14:04:45 +0100

    spectroscopy.py: Add possibility for 2D log parameters

--- qkit/measure/spectroscopy/spectroscopy.py
+++ qkit/measure/spectroscopy/spectroscopy.py
@@ -135,6 +135,41 @@ class spectrum(object):
                 self.log_name.append(name[i])
                 self.log_unit.append(unit[i])
                 self.log_dtype.append(log_dtype[i])
+    
+    def set_log_function_2D(self, func=None, name=None, unit=None, y=None, y_name=None, y_unit=None, log_dtype=None):
+        if name == None:
+            try:
+                name = ['log_param'] * len(func)
+            except Exception:
+                name = None
+        if unit == None:
+            try:
+                unit = [''] * len(func)
+            except Exception:
+                unit = None
+        if log_dtype == None:
+            try:
+                log_dtype = ['f'] * len(func)
+            except Exception:
+                log_dtype = None
+
+        self.log_function_2D = []
+        self.log_name_2D = []
+        self.log_unit_2D = []
+        self.log_y_2D = []
+        self.log_y_name_2D = []
+        self.log_y_unit_2D = []
+        self.log_dtype_2D = []
+
+        if func != None:
+            for i, _ in enumerate(func):
+                self.log_function_2D.append(func[i])
+                self.log_name_2D.append(name[i])
+                self.log_unit_2D.append(unit[i])
+                self.log_dtype_2D.append(log_dtype[i])
+                self.log_y_2D.append(y[i])
+                self.log_y_name_2D.append(y_name[i])
+                self.log_y_unit_2D.append(y_unit[i])
 
     def set_x_parameters(self, x_vec, x_coordname, x_set_obj, x_unit=""):
         """
@@ -279,6 +314,22 @@ class spectrum(object):
                     self._log_value.append(
                         self._data_file.add_value_vector(self.log_name[i], x=self._data_x, unit=self.log_unit[i],
                                                          dtype=self.log_dtype[i]))
+
+
+            if self.log_function_2D != None:  # use 2D logging
+                self._log_y_value_2D = []
+                for i in range(len(self.log_y_name_2D)):
+                    if self.log_y_name_2D[i] not in self.log_y_name_2D[:i]:  # add y coordinate for 2D logging
+                        self._log_y_value_2D.append(self._data_file.add_coordinate(self.log_y_name_2D[i], unit=self.log_y_unit_2D[i]))
+                        self._log_y_value_2D[i].add(self.log_y_2D[i])
+                    else:  # use y coordinate for 2D logging if it is already added
+                        self._log_y_value_2D.append(self._log_y_value_2D[np.squeeze(np.argwhere(self.log_y_name_2D[i] == np.array(self.log_y_name_2D[:i])))])
+                
+                self._log_value_2D = []
+                for i in range(len(self.log_function_2D)):
+                    self._log_value_2D.append(
+                        self._data_file.add_value_matrix(self.log_name_2D[i], x=self._data_x, y=self._log_y_value_2D[i], unit=self.log_unit_2D[i],
+                                                         dtype=self.log_dtype_2D[i]))
 
         if self.comment:
             self._data_file.add_comment(self.comment)
@@ -512,6 +563,10 @@ class spectrum(object):
                 if self.log_function != None:
                     for i, f in enumerate(self.log_function):
                         self._log_value[i].append(float(f()))
+
+                if self.log_function_2D != None:
+                    for i, f in enumerate(self.log_function_2D):
+                        self._log_value_2D[i].append(f())
 
                 if self._scan_dim == 3:
                     for y in self.y_vec:
