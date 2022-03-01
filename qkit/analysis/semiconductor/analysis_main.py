dc62c971f794324533d287ac866b5259cc43f092
AngryRabbitChicken <julian.ferrero@kit.edu>
2022-03-01 19:17:47 +0100

    Building the dream. WIP. Best commit message EU West

--- qkit/analysis/semiconductor/analysis_main.py
+++ qkit/analysis/semiconductor/analysis_main.py
@@ -1,8 +1,17 @@
+from importlib.machinery import ModuleSpec
 import sys
 import os
+import importlib.util
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QMainWindow, QLineEdit, QApplication, QVBoxLayout, QGridLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QMainWindow, QLineEdit, QApplication, QVBoxLayout, QGridLayout, QLabel, QPushButton, QFileDialog
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
+
+test_mode = True
+if test_mode:
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from itertools import cycle
 
 class Settings_tab(QWidget):
     def __init__(self, obj):
@@ -35,18 +44,143 @@ class Settings_window(QWidget):
             new_tab = Settings_tab(obj)
             self.tabWidget.addTab(new_tab, type(obj).__name__)
 
+class Mpl_canvas(FigureCanvasQTAgg):
+    def __init__(self, plotter) -> None:
+        self.plotter = plotter
+        figure = self.plotter.fig
+        self.axes = figure.gca()
+        super().__init__(figure)
+
+    def update_plot(self):
+        self.axes.cla()
+        self.plotter.validate_input()
+        self.plotter.plot()
+        self.draw()
+
+class Plot_widget(QWidget):
+    def __init__(self, plotter):
+        super().__init__()
+        
+        self.canvas = Mpl_canvas(plotter)
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
+
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.toolbar)
+        self.vbox.addWidget(self.canvas)
+
+        self.canvas.draw()
+
+class Plot_window(QWidget):
+    def __init__(self, plotter):
+        super().__init__()
+        uic.loadUi(os.path.join("main", "ui", "plot_window.ui"), self)
+
+        self.plot_widget = Plot_widget(plotter)
+
+        self.layout().addWidget(self.plot_widget)
+        self.plot_widget.canvas.update_plot()
+
+class FileDialog_window(QFileDialog):
+    def __init__(self):
+        super().__init__()
+        print(self.getOpenFileName())
+
 class Mainframe(QMainWindow):
     def __init__(self, *objects):
         super().__init__()
         uic.loadUi(os.path.join("main", "ui", "main_window.ui"), self)
+        print(self.__dict__)
         self.objects = objects
-        self.button_settings.clicked.connect(self.open_settings_window)    
+        self.plotter = objects[-1]
+
+        self.plot_widget = Plot_widget(self.plotter)
+
+        self.button_load.clicked.connect(self.load_data)
+        self.button_add_files.clicked.connect(self.add_files)
+        self.button_reset_files.clicked.connect(self.reset_files)
+
+        self.button_settings.clicked.connect(self.open_settings_window)
+        
+        self.button_analyze.clicked.connect(self.analyze_data)
+        self.button_plot.clicked.connect(self.open_plot_window)
+        
+        self.action_Save_Settings.triggered.connect(self.save_settings)
+        self.action_Load_Settings.triggered.connect(self.load_settings)
+        self.action_Choose_Loader.triggered.connect(self.choose_loader)
+        self.action_Choose_Analyzer.triggered.connect(self.choose_analyzer)
+        self.action_Choose_Plotter.triggered.connect(self.choose_plotter)
+
+        self.loader = None
+        self.analyzer = None
+        self.plotter = None
+
+        self.files = []
+        self.data_raw = None
+        self.data_analyzed = None
+    
+    @staticmethod
+    def load_module_from_filepath(fname):
+        module_name = os.path.splitext(os.path.basename(fname))[0]
+        module_spec = importlib.util.spec_from_file_location(module_name, fname)
+        module = importlib.util.module_from_spec(module_spec) #type: ignore
+        module_spec.loader.exec_module(module) #pyright: reportOptionalMemberAccess=false
+        return module
+
+    def save_settings(self):
+        raise NotImplementedError("Still gotta finish this bad boy. Your princess is in another castle.")
+    
+    def load_settings(self):
+        raise NotImplementedError("Still gotta finish this bad boy. Your princess is in another castle.")
+
+    def choose_loader(self):
+        file = "loader"
+        fname, _ = QFileDialog.getOpenFileName(self, f"Choose {file}", os.path.join(f"{file}s"), "Python files(*py)")
+        if fname:
+            module = self.load_module_from_filepath(fname)
+            self.loader = module.Loader()
+
+    def choose_analyzer(self):
+        file = "analyzer"
+        fname, _ = QFileDialog.getOpenFileName(self, f"Choose {file}", os.path.join(f"{file}s"), "Python files(*py)")
+        if fname:
+            module = self.load_module_from_filepath(fname)
+            self.analyzer = module.Analyzer()
+    
+    def choose_plotter(self):
+        file = "plotter"
+        fname, _ = QFileDialog.getOpenFileName(self, f"Choose {file}", os.path.join(f"{file}s"), "Python files(*py)")
+        if fname:
+            module = self.load_module_from_filepath(fname)
+            self.plotter = module.Plotter()
     
     def open_settings_window(self):
         self.settings_window = Settings_window(*self.objects)
         self.settings_window.show()
 
-class Test_plotter():
+    def add_files(self):
+        fnames, _ = QFileDialog.getOpenFileNames(self, "Choose files to load")
+        for name in fnames:
+            self.text_browser_file_display.append(os.path.basename(name))
+        self.files.extend(fnames)
+
+    def reset_files(self):
+        self.files = []
+        self.text_browser_file_display.clear()
+
+    def load_data(self):
+        self.loader.set_filepath(self.files)
+        self.data_raw = self.loader.load()    
+
+    def analyze_data(self):
+        self.analyzer.load_data(self.data_raw)
+        self.analyzer.validate_input()
+        self.data_analyzed = self.analyzer.analyze()
+
+    def open_plot_window(self):
+        self.plot_window = Plot_window(self.plotter)
+        self.plot_window.show()
+
+class Attribute_container():
     class_attr1 = 1
     def __init__(self) -> None:
         self.attr1 = 1
@@ -59,24 +193,27 @@ class Test_plotter():
         self.Viktor = "LoLBrot"
         pass
 
-def main():
-    plottr = Test_plotter()
+class Test_plotter:
+    def __init__(self) -> None:
+        self.cycler = cycle("rgb")
+        self.fig = plt.Figure()
+        self.fig.subplots()
+        self.ax = self.fig.gca()
+        self.x = np.linspace(0, 2, 100)
+        self.y = self.x**2
+        
+    def validate_input(self):
+        pass
+    
+    def plot(self):
+        self.ax.plot(self.x, self.y, next(self.cycler))
 
-    plottr.Viktor ="kek"
-    plottr2 = Test_plotter()
-    Test_plotter.class_attr1 = 12
-    plottr3 = Test_plotter()
-    plottr4 = Test_plotter()
-    plottr2.attr1 = 67
-    print(plottr.attr1)
-    print(plottr2.attr1)
-    print(f"plottr1 class_attr1: {plottr.class_attr1}")
-    print(f"plottr2 class_attr1: {plottr2.class_attr1}")
-    print(f"plottr3 class_attr1: {plottr3.class_attr1}")
-    print(f"plottr4 class_attr1: {plottr4.class_attr1}")
+def main():
+    obj1 = Attribute_container()
+    plottr = Test_plotter()   
 
     app = QApplication([])
-    mainWin = Mainframe(plottr, plottr2)
+    mainWin = Mainframe(obj1, plottr)
     mainWin.show()
 
     sys.exit(app.exec_())
--- qkit/analysis/semiconductor/interfaces.py
+++ qkit/analysis/semiconductor/interfaces.py
@@ -11,6 +11,10 @@ class PlotterInterface(ABC):
 
 class AnalyzerInterface(ABC):
     @abstractmethod
+    def load_data():
+        pass
+
+    @abstractmethod
     def validate_input():
         pass
 
@@ -19,6 +23,10 @@ class AnalyzerInterface(ABC):
         pass
 
 class LoaderInterface(ABC):
+    @abstractmethod
+    def set_filepath():
+        pass
+    
     @abstractmethod
     def load():
         pass
