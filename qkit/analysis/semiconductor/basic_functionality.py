3ac869f75b01174b26fc63b86e47374f27b9a208
phi-nte <phi-nte@phi.kit.edu>
2022-04-20 17:32:41 +0200

    update

--- qkit/analysis/semiconductor/basic_functionality.py
+++ qkit/analysis/semiconductor/basic_functionality.py
@@ -24,11 +24,18 @@ def convert_conductance(amplitudes, settings, multiplier):
 def map_array_to_index(array, value):
     """Takes an array of SORTED values and returns the nearest index where the value is found. 
     """
-    idx = np.searchsorted(array, value, side="left")
-    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
-        return idx-1
-    else:
+    if array[0]<array[-1]: # array in ascending order
+        idx = np.searchsorted(array, value, side="left")
+        #if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        #    return idx-1
+        #else:
         return idx
+    else: # array in descending order
+        idx = np.searchsorted(array[::-1], value, side="left")
+        #if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        #   return len(array) - idx - 1
+        #else:
+        return len(array) - idx
 
     
 def convert_secs(timestamps, f=1.8*1e9):
