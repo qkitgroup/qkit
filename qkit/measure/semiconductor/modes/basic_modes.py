4460b4a588936276476a69075353f3f7c0f34327
AngryRabbitChicken <julian.ferrero@kit.edu>
2023-06-16 14:22:11 +0200

    PpvsT added

--- qkit/measure/semiconductor/modes/basic_modes.py
+++ qkit/measure/semiconductor/modes/basic_modes.py
@@ -96,13 +96,14 @@ class PpvsT(ModeBase):
         self.measurement_settings = measurement_settings
         self.unit = "a.u."
         self.tag = self.create_tag()
-        self.column = makehash()
+        self.total_sum = makehash()
+        self.divider = makehash()
         self.reset()
 
     def create_coordinates(self):
         for name, measurement in self.measurement_settings.items():
-            self.fh.update_coordinates(f"{self.tag}.iter", np.arange(measurement["averages"] * measurement["measurement_count"]), 
-                                        f"{self.tag}:iterations.{name}",
+            self.fh.update_coordinates(f"{self.tag}.pp", measurement["loop_range_pp"], 
+                                        f"{self.tag}:{measurement['loop_step_name_pp']}.{name}",
                                         name)
             self.fh.update_coordinates(f"{self.tag}.tt", measurement["loop_range_tt"], 
                                         f"{self.tag}:{measurement['loop_step_name_tt']}.{name}",
@@ -111,8 +112,8 @@ class PpvsT(ModeBase):
     def create_datasets(self, additional_coords):
         for name, measurement in self.measurement_settings.items():
             for node in measurement["data_nodes"]:
-                coords = additional_coords + [self.fh.multiplexer_coords[f"{self.tag}.iter"][name], self.fh.multiplexer_coords[f"{self.tag}.tt"][name]]
-                self.fh.add_dset(f"{self.tag}:{name}.{node}", coords, self.unit)
+                coords = additional_coords + [self.fh.multiplexer_coords[f"{self.tag}.pp"][name], self.fh.multiplexer_coords[f"{self.tag}.tt"][name]]
+                self.fh.add_dset(f"{self.tag}:{name}.{node}", coords, measurement["unit"])
 
     def fill_file(self, latest_data, data_location):
         for measurement_name, node_values in latest_data.items():
@@ -120,17 +121,15 @@ class PpvsT(ModeBase):
             first_node = list(node_values.keys())[0]
             collected_averages = len(node_values[first_node])
             if collected_averages== 0: continue
+            self.divider[measurement_name] += collected_averages
             for node_name, node_value in node_values.items():
-                for grid in node_value:
-                    for single_trace in grid:
-                        position = data_location + (self.column[measurement_name][node_name],)
-                        self.fh.write_to_file(f"{self.tag}:{measurement_name}.{node_name}", single_trace, 
-                        position)
-                        self.column[measurement_name][node_name] += 1
+                self.total_sum[measurement_name][node_name] += np.sum(node_value, axis = 0)
+                value = self.total_sum[measurement_name][node_name]/self.divider[measurement_name]
+                self.fh.write_to_file(f"{self.tag}:{measurement_name}.{node_name}", value, data_location)
     
     def reset(self):
         for name, measurement in self.measurement_settings.items():
+            self.divider[name] = 0
             for node in measurement["data_nodes"]:
-                self.column[name][node] = 0
-#TODO: Add PpvsT
+                self.total_sum[name][node] = 0
 #TODO: Add TimeTrace
