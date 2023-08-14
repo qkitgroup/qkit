2fc6bef2aa842bc11d5ef6867e6841b8d45e3d72
“MichaWildermuth” <Micha.Wildermuth@kit.edu>
2023-08-14 21:59:04 +0200

    pcf8575.py: string formatting

--- src/qkit/services/qplexkit/pcf8575.py
+++ src/qkit/services/qplexkit/pcf8575.py
@@ -12,20 +12,20 @@ class IOPort(list):
 
     def __setitem__(self, key, value):
         """
-        Set an individual output pin.
+        Sets an individual output pin to value.
         """
         self.pcf8575.set_output(key, value)
         logging.info(f'set port {key} to {value}')
 
     def __getitem__(self, key):
         """
-        Get an individual pin state.
+        Gets an individual pin state.
         """
-        return self.pcf8575.get_pin_state(key)
+        return self.pcf8575.get_output(key)
 
     def __repr__(self):
         """
-        Represent port as a list of booleans.
+        Represent all ports as a list of booleans.
         """
         state = self.pcf8575.bus.read_word_data(self.pcf8575.address, 0)
         ret = []
@@ -60,14 +60,14 @@ class PCF8575(object):
     @property
     def port(self):
         """
-        Represent IO port as a list of boolean values.
+        Represent IO ports as a list of boolean values.
         """
         return IOPort(self)
 
     @port.setter
     def port(self, value):
         """
-        Set the whole port using a list.
+        Set all ports using a list.
         """
         assert isinstance(value, list)
         assert len(value) == 16
@@ -77,20 +77,20 @@ class PCF8575(object):
                 new_state |= 1 << 15 - i
         self.bus.write_byte_data(self.address, new_state & 0xff, (new_state >> 8) & 0xff)
 
-    def set_output(self, output_number, value):
+    def set_output(self, pin, value):
         """
-        Set a specific output high (True) or low (False).
+        Sets a specific pin output to high (True) or low (False).
         """
-        assert output_number in range(16), "Output number must be an integer between 0 and 15"
+        assert pin in range(16), "Output number must be an integer between 0 and 15"
         current_state = self.bus.read_word_data(self.address, 0)
-        bit = 1 << 15 - output_number
+        bit = 1 << 15 - pin
         new_state = current_state | bit if value else current_state & (~bit & 0xffff)
         self.bus.write_byte_data(self.address, new_state & 0xff, (new_state >> 8) & 0xff)
 
-    def get_pin_state(self, pin_number):
+    def get_output(self, pin):
         """
-        Get the boolean state of an individual pin.
+        Gets the boolean state of an individual pin.
         """
-        assert pin_number in range(16), "Pin number must be an integer between 0 and 15"
+        assert pin in range(16), "Pin number must be an integer between 0 and 15"
         state = self.bus.read_word_data(self.address, 0)
-        return bool(state & 1 << 15 - pin_number)
+        return bool(state & 1 << 15 - pin)
