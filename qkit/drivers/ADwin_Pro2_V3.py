aea21445302c6378f3de1721ea1171a7511c4ac1
Daniel <daniel.schroller@kit.edu>
2023-11-27 16:48:44 +0100

    Important line missed in Adwin driver for triggered readout

--- qkit/drivers/ADwin_Pro2_V3.py
+++ qkit/drivers/ADwin_Pro2_V3.py
@@ -1798,6 +1798,7 @@ class ADwin_Pro2_V3(Instrument):
             data_volts = data_volts_averaged
 
         data_triggered_readout = np.empty(shape=(1, self.measurement_count, self.sample_count))
+        data_triggered_readout[0:1, :, :] = data_volts
         self.start_triggered_readout()  # starting new average
         return data_triggered_readout
 
