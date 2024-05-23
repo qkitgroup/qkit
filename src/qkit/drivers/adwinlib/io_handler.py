0e50d0a40efbca97458ae8506e81e230780cc2d7
Luca Kosche <luca.kosche@kit.edu>
2024-05-23 13:02:37 +0200

    updated module docstring

--- src/qkit/drivers/adwinlib/io_handler.py
+++ src/qkit/drivers/adwinlib/io_handler.py
@@ -20,20 +20,22 @@
 
 EXAMPLE CONFIGS:
 
+# 'hard wired' configuration of the adwin and accessories
 hard_config = {
     'no_output_channels': 8,
     'outputs': {
-        'bx': {'card': 3, 'channel': 1, 'scale': 0.995, 'unit': 'T'},
-        'by': {'card': 3, 'channel': 3, 'scale': 0.945, 'unit': 'T'},
-        'bz': {'card': 3, 'channel': 5, 'scale': 1.265, 'unit': 'T'},
-        'vg': {'card': 3, 'channel': 7, 'scale': 10, 'unit': 'V'},
-        'vd': {'card': 3, 'channel': 8, 'scale': 10, 'unit': 'V'}
+        'bx': {'card': 3, 'channel': 1, 'scale': 0.995, 'unit': 'T', 'bits':16},
+        'by': {'card': 3, 'channel': 3, 'scale': 0.945, 'unit': 'T', 'bits':16},
+        'bz': {'card': 3, 'channel': 5, 'scale': 1.265, 'unit': 'T', 'bits':16},
+        'vg': {'card': 3, 'channel': 7, 'scale': 10, 'unit': 'V', 'bits':16},
+        'vd': {'card': 3, 'channel': 8, 'scale': 10, 'unit': 'V', 'bits':16}
         },
     'inputs': {
-        'id': {'channel': 8, 'scale': 10, 'unit': 'A', 'bits': 18}
+        'id': {'card': 2, 'channel': 8, 'scale': 10, 'unit': 'A', 'bits': 18}
         }
     }
 
+# between measurement 'switchable' configuration of adwin accessories 
 soft_config = {
     'vdivs': {'vg':0.5, 'vd':0.01},
     'iv_gain': {'id':1e8},
