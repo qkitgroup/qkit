4e2191fd335d8bec17dca513d803540313d27389
Marius-Frohn <uzrfo@student.kit.edu>
2024-08-06 18:20:54 +0200

    Split IVVI and communication
    New version with serial port communication and IVVI logic split into separate files for usage of derial port drivers for other projects

--- src/qkit/drivers/IVVI_BiasDAC.py
+++ src/qkit/drivers/IVVI_BiasDAC.py
@@ -5,13 +5,13 @@ For more information about the IVVI visit https://qtwork.tudelft.nl/~schouten/iv
 The underlying communication protocol is explained at https://qtwork.tudelft.nl/~schouten/ivvi/doc-d5/rs232linkformat.txt .
 
 Improvements compared to legacy codebase include
-- added serial port connection possibility
+- added various serial port connection types in SerialPortCommunication module
 - underlying driver version readback
-- internal book-keeping of set DAC-range-knobs using helper class IVVI_DAC_Group to allow dynamically adjusted conversions at different DACs
+- internal book-keeping of set DAC-range-knobs using helper class IVVI_DAC_Group to allow adjusted conversions at different DACs
 - enhanced qkit conformity and integration
 
 Author: Marius Frohn <uzrfo@student.kit.edu>
-Version: 2.2; (07/2024) 
+Version: 2.3; (07/2024) 
 
 (Legacy DOC-string)
 IVVI.py class, to perform the communication between the Wrapper and the device
@@ -19,10 +19,11 @@ Pieter de Groot <pieterdegroot@gmail.com>, 2008
 Martijn Schaafsma <qtlab@mcschaafsma.nl>, 2008
 Reinier Heeres <reinier@heeres.eu>, 2008
 
-extended by Tim Wolz to access the IVVI via Ethernet.
+Extended by Tim Wolz to access the IVVI via Ethernet.
 Data is sent as a string via Ethernet to a Raspberry Pi, 
 where it is converted to asci code and sent to the IVVI
 Ethernet connection based on the lazy pirate pattern by Daniel Lundin <dln(at)eintr(dot)org> 
+(Note Marius: Moved to SerialPortCommunication.SerialPortRasbpi)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -39,48 +40,51 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-import logging, serial, serial.tools.list_ports, serial.serialutil, time, zmq
+import logging
 from qkit.core.instrument_base import Instrument
-
-class IVVI_DAC_Group:
-    """
-    Helper class for keeping track of set DAC ranges at IVVI D5 module and binary <-> mV conversion
-    IVVI can switch DAC polization between negative, centered around 0 and positive in groups of 4. 
-    """
-    def __init__(self, val1: float, val2: float):
-        self.low = min(val1, val2)
-        self.high = max(val1, val2)
-    def __str__(self) -> str:
-        return "IVVI DAC range helper object for {} to {} mV".format(self.low, self.high)
-    def mv2bytes(self, mvolt: float) -> tuple[int, int]:
-        if mvolt < self.low or mvolt > self.high:
-            raise ValueError
-        else:
-            bytevalue = int(round(65535*(mvolt - self.low)/(self.high - self.low)))
-            return (bytevalue//256, bytevalue % 256)
-    def bytes2mv(self, byte_high: int, byte_low: int) -> float:
-        # Correct readback expected, no value checking
-        return self.low + (self.high - self.low)*(256*byte_high + byte_low)/65536
-
-# available ranges
-DACRangeNeg = IVVI_DAC_Group(-4000, 0)
-DACRangeBip = IVVI_DAC_Group(-2000, 2000)
-DACRangePos = IVVI_DAC_Group(0, 4000)
-RangeIdent = { "pos": DACRangePos, "bip": DACRangeBip, "neg": DACRangeNeg, 
-               "Pos": DACRangePos, "Bip": DACRangeBip, "Neg": DACRangeNeg, 
-               "POS": DACRangePos, "BIP": DACRangeBip, "NEG": DACRangeNeg, 
-               "+"  : DACRangePos, "0"  : DACRangeBip, "-"  : DACRangeNeg  }
-
+import SerialPortCommunication as SPC
 
 class IVVI_BiasDAC(Instrument):
     """
-    This driver provides access to the IVVI's bias DACs
-    It can either be used via serial port with a direct cable connection or via ethernet if i.e. a RasbPi is set up as a man-in-the-middle
+    This driver provides access to the IVVI's bias DACs.
+    Usage requires communicating with the IVVI's control box via RS232 serial port connection.
+    The SerialPortCommunication module/instrument is intended to do that.
 
     Initialize with
-    <name> = instruments.create('<name>', 'IVVI_BiasDAC', <"Ethernet"/"SerialPort">, **kwargs)
+    <SPC_name> = instruments.create('<SPC_name>', 'SerialPortCommunication', *args for creating connection*)
+    <name> = instruments.create('<name>', 'IVVI_BiasDAC', connection=<SPC_name>, **kwargs)
     """
-    def __init__(self, name: str, connection_type: str, knob_config: list[str] = ["0", "0", "0", "0"], reset_on_init: bool = True, **kwargs):
+
+    class IVVI_DAC_Group:
+        """
+        Helper class for keeping track of set DAC ranges at IVVI D5 module and binary <-> mV conversion
+        IVVI can switch DAC polization between negative, centered around 0 and positive in groups of 4. 
+        """
+        def __init__(self, val1: float, val2: float):
+            self.low = min(val1, val2)
+            self.high = max(val1, val2)
+        def __str__(self) -> str:
+            return "IVVI DAC range helper object for {} to {} mV".format(self.low, self.high)
+        def mv2bytes(self, mvolt: float) -> tuple[int, int]:
+            if mvolt < self.low or mvolt > self.high:
+                raise ValueError
+            else:
+                bytevalue = int(round(65535*(mvolt - self.low)/(self.high - self.low)))
+                return (bytevalue//256, bytevalue % 256)
+        def bytes2mv(self, byte_high: int, byte_low: int) -> float:
+            # Correct readback expected, no value checking
+            return self.low + (self.high - self.low)*(256*byte_high + byte_low)/65536
+
+    # available ranges
+    DACRangeNeg = IVVI_DAC_Group(-4000, 0)
+    DACRangeBip = IVVI_DAC_Group(-2000, 2000)
+    DACRangePos = IVVI_DAC_Group(0, 4000)
+    RangeIdent = { "pos": DACRangePos, "bip": DACRangeBip, "neg": DACRangeNeg, 
+                "Pos": DACRangePos, "Bip": DACRangeBip, "Neg": DACRangeNeg, 
+                "POS": DACRangePos, "BIP": DACRangeBip, "NEG": DACRangeNeg, 
+                "+"  : DACRangePos, "0"  : DACRangeBip, "-"  : DACRangeNeg  }
+
+    def __init__(self, name: str, connection: SPC.SerialPortCommunication, knob_config: list[str] = ["0", "0", "0", "0"], reset_on_init: bool = True):
         """
         Initialzes the IVVI Bias DAC by communicating via serial port
 
@@ -100,18 +104,12 @@ class IVVI_BiasDAC(Instrument):
             None
         """
         Instrument.__init__(self, name, tags=['physical'])
-        if connection_type == "SerialPort":
-            self.connection = IVVI_SerialPort(**kwargs)
-        elif connection_type == "Ethernet":
-            self.connection = IVVI_Ethernet(**kwargs)
-        else:
-            print("Unknown connection_type '{}'. Needs to be 'Ethernet' or 'SerialPort'".format(connection_type))
-            raise ValueError
+        self.connection = connection
         for j in range(16):
             self.__dict__["do_set_DAC_{}_voltage".format(j + 1)] = lambda mVolt, j=j: self.set_dac(j + 1, mVolt)
             self.__dict__["do_get_DAC_{}_voltage".format(j + 1)] = lambda j=j: self.get_dac(j + 1)
-            self.add_parameter("DAC_{}_voltage".format(j + 1), type=float, minval=DACRangeBip.low, maxval=DACRangeBip.high, units='mV')
-        self.dac_ranges = [DACRangeBip] * 4
+            self.add_parameter("DAC_{}_voltage".format(j + 1), type=float, minval=self.DACRangeBip.low, maxval=self.DACRangeBip.high, units='mV')
+        self.dac_ranges = [self.DACRangeBip] * 4
         for i in range(4):
             self.set_dacgroup_range(i, knob_config[i])
         self.add_function("set_dacgroup_range")
@@ -133,6 +131,33 @@ class IVVI_BiasDAC(Instrument):
         """
         del self.connection
         logging.info(__name__ + ': Deleting IVVI instrument')
+    
+    @staticmethod
+    def handle_readback_error_flag(readback: list[int]) -> list[int]:
+        """
+        Readback of every command sent to IVVI-rack has the format [*Readback length*, *Error flag byte*, DataByte, DataByte, ...].
+        This function checks if readback has in fact the size it claims to have and interprets the error flag.
+        It strips these two bytes from the readback and returns only the data bytes.
+
+        Input:
+            readback (list[int]): Readback from a communication with IVVI-rack
+
+        Output:
+            (list[int]): Data bytes of given readback
+        """
+        if len(readback) != readback[0]:
+            logging.info(__name__ + ": Unknown communication error")
+            print(__name__ + ": Unknown communication error")
+        if readback[1] == 32:
+            logging.info(__name__ + ': Watchdog reset detected')
+            print(__name__ + ": Watchdog reset detected")
+        elif readback[1] == 64:
+            logging.info(__name__ + ': DAC does not exist')
+            print(__name__ + ": DAC does not exist")
+        elif readback[1] == 128:
+            logging.info(__name__ + ': wrong action')
+            print(__name__ + ": wrong action")
+        return readback[2:]
       
     def set_dacgroup_range(self, group_num: int, ident: str):
         """
@@ -146,12 +171,12 @@ class IVVI_BiasDAC(Instrument):
         Output:
             None
         """
-        if not ident in RangeIdent:
+        if not ident in self.RangeIdent:
             print("Given identifier '{}' unkown. Try 'pos', 'bip', 'neg' instead. Group range unchanged".format(ident))
         elif group_num < 0 or group_num >= len(self.dac_ranges):
             print("DAC group number {} out of range 0 .. 3. Group range unchanged".format(group_num))
         else:
-            self.dac_ranges[group_num] = RangeIdent[ident]
+            self.dac_ranges[group_num] = self.RangeIdent[ident]
             for i in range(4):
                 self.set_parameter_options("DAC_{}_voltage".format(4*group_num + i + 1), minval=self.dac_ranges[group_num].low, maxval=self.dac_ranges[group_num].high)
     
@@ -180,7 +205,7 @@ class IVVI_BiasDAC(Instrument):
             None
         """
         try:
-            self.connection.communicate([7, 0, 2, 1, channel] + list(self.dac_ranges[(channel - 1)//4].mv2bytes(mVolt)))
+            self.handle_readback_error_flag(self.connection.communicate([7, 0, 2, 1, channel] + list(self.dac_ranges[(channel - 1)//4].mv2bytes(mVolt)), 2))
         except ValueError:
             print("Given voltage {} mV not within selected DAC-range {} .. {} mV".format(mVolt, self.dac_ranges[(channel - 1)//4].low, self.dac_ranges[(channel - 1)//4].high))
 
@@ -194,7 +219,7 @@ class IVVI_BiasDAC(Instrument):
         Output:
             (list[int]) : raw DAC values as [DAC_1_byte_high, DAC_1_byte_low, DAC_2_byte_high, ...]
         """
-        return self.connection.communicate([4, 0, 34, 2])
+        return self.handle_readback_error_flag(self.connection.communicate([4, 0, 34, 2], 34))
     
     def get_dacs_list(self) -> list[float]:
         """
@@ -235,172 +260,6 @@ class IVVI_BiasDAC(Instrument):
         # For some unknown reason reading out the driver version outmatically sets it as high byte in DAC1
         # This call fixes this error in an ugly way
         dac_val = self.get_dac(1)
-        drive = self.connection.communicate([4, 0, 3, 4])[0]
+        drive = self.handle_readback_error_flag(self.connection.communicate([4, 0, 3, 4], 3))[0]
         self.set_dac(1, dac_val)
         return drive
-
-
-class IVVI_SerialPort:
-    """
-    Helper class handling communication via serial port
-    Possible port names can be found before initialization with get_ports()
-    """
-    @staticmethod
-    def get_ports():
-        """
-        Shortcut for serial.tools.list_ports.main()
-        Implemented as static function so one can determine to be used port name before instrument creation
-
-        Input:
-            None
-        
-        Output:
-            (None): prints information about currently available serial ports
-        """
-        serial.tools.list_ports.main()
-    
-    def __init__(self, port: str = "COM3", timeout: float = 3, timeskip: float = 0.1):
-        self.timeskip = timeskip
-        try:
-            self.ser = serial.Serial(port=port, baudrate=115200, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, timeout=timeout, write_timeout=timeout)
-        except serial.serialutil.SerialException:
-            print("Error while trying to open serial port '{}' for IVVI. Port not connected?".format(port))
-            raise serial.serialutil.SerialException
-        self.ser.close()
-    
-    def __del__(self):
-        self.ser.close()
-    
-    def communicate(self, message: list[int]) -> list[int]:
-        """
-        Main communication protocol of the serial port connection with integrated error flag interpretation
-
-        Input:
-            message (list[int]) : to be sent message
-        
-        Output:
-            (list[int]) : received response, stripped off size & error flag bytes 
-        """
-        # print(message) # for debugging
-        self.ser.open()
-        if self.ser.in_waiting > 0:
-            # clear input if something still in waiting for whetever reason
-            self.ser.read(self.ser.in_waiting)
-            time.sleep(self.timeskip)
-        self.ser.write(bytes(message))
-        time.sleep(self.timeskip)
-        readback = list(self.ser.read(self.ser.in_waiting))
-        if len(readback) != readback[0]:
-            logging.info(__name__ + ": Unknown communication error")
-            print(__name__ + ": Unknown communication error")
-        if readback[1] == 32:
-            logging.info(__name__ + ': Watchdog reset detected')
-            print(__name__ + ": Watchdog reset detected")
-        elif readback[1] == 64:
-            logging.info(__name__ + ': DAC does not exist')
-            print(__name__ + ": DAC does not exist")
-        elif readback[1] == 128:
-            logging.info(__name__ + ': wrong action')
-            print(__name__ + ": wrong action")
-        time.sleep(self.timeskip)
-        self.ser.close()
-        # print(readback) # for debugging
-        return readback[2:]
-    
-
-class IVVI_Ethernet:
-    """
-    Helper class handling communication via ethernet
-    """
-    def __init__(self, address='10.22.197.115'):
-        self._address = address
-        self._port = 6543
-        self.REQUEST_TIMEOUT = 2500
-        self.REQUEST_RETRIES = 3
-        self._open_zmq_connection()
-
-    def __del__(self):
-        self._close_zmq_connection()
-
-    def _open_zmq_connection(self):
-        """
-        Connects to the raspberry via zmq/tcp
-
-        Input:
-            None
-
-        Output:
-            None
-        """
-        self.context = zmq.Context(1)
-        print("Connecting to Raspberry Pi")
-        self.client = self.context.socket(zmq.REQ)
-        self.client.connect("tcp://%s:%s" % (self._address,self._port)) # raspi address
-        self.poll = zmq.Poller()
-        self.poll.register(self.client, zmq.POLLIN)
-        
-    def _close_zmq_connection(self):
-        """
-        Closes the zmq connection
-
-        Input:
-            None
-
-        Output:
-            None
-        """
-        logging.debug(__name__ + ' : Closing ethernet connection')
-        self.context.term()
-
-    def communicate(self, message: list[int]) -> list[int]:
-        """
-        Main communication protocol of the ethernet connection with integrated error flag interpretation
-
-        Input:
-            message (list[int]) : to be sent message
-        
-        Output:
-            (list[int]) : received response, stripped off size & error flag bytes 
-        """
-        logging.debug(__name__ + ' : do communication with instrument')
-        message = ("{} "*message[0]).format(*message)
-        retries_left = self.REQUEST_RETRIES
-        while retries_left:
-            self.client.send(message)
-            expect_reply = True
-            while expect_reply:
-                socks = dict(self.poll.poll(self.REQUEST_TIMEOUT))
-                if socks.get(self.client) == zmq.POLLIN:
-                    data_out_string = self.client.recv()
-                    if not data_out_string:
-                        break
-                    else:
-                        retries_left = 0
-                        expect_reply = False
-                else:
-                    print("No response from server, retrying")
-                    # Socket is confused. Close and remove it.
-                    self.client.setsockopt(zmq.LINGER, 0)
-                    self.client.close()
-                    self.poll.unregister(self.client)
-                    retries_left -= 1
-                    if retries_left == 0:
-                        print("Server seems to be offline, abandoning")
-                        break
-                    print("Reconnecting and resending " + message)
-                    # Create new connection
-                    self._open_zmq_connection()
-        readback = [int(s) for s in data_out_string.split(' ')]
-        if len(readback) != readback[0]:
-            logging.info(__name__ + ": Unknown communication error")
-            print(__name__ + ": Unknown communication error")
-        if readback[1] == 32:
-            logging.info(__name__ + ': Watchdog reset detected')
-            print(__name__ + ": Watchdog reset detected")
-        elif readback[1] == 64:
-            logging.info(__name__ + ': DAC does not exist')
-            print(__name__ + ": DAC does not exist")
-        elif readback[1] == 128:
-            logging.info(__name__ + ': wrong action')
-            print(__name__ + ": wrong action")
-        return readback[2:]
