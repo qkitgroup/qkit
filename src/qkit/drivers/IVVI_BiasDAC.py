ed89d6c9596b19622a6d12f86e83d29378c03ba6
Marius-Frohn <uzrfo@student.kit.edu>
2024-08-13 17:04:25 +0200

    Untested Dummy IVD within IVVI driver

--- src/qkit/drivers/IVVI_BiasDAC.py
+++ src/qkit/drivers/IVVI_BiasDAC.py
@@ -9,9 +9,10 @@ Improvements compared to legacy codebase include
 - underlying driver version readback
 - internal book-keeping of set DAC-range-knobs using helper class IVVI_DAC_Group to allow adjusted conversions at different DACs
 - enhanced qkit conformity and integration
+- dummy IVD for sweeping given ranges in different modes
 
 Author: Marius Frohn <uzrfo@student.kit.edu>
-Version: 2.3; (07/2024) 
+Version: 2.4; (08/2024) 
 
 (Legacy DOC-string)
 IVVI.py class, to perform the communication between the Wrapper and the device
@@ -40,9 +41,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-import logging
+import logging, time
 from qkit.core.instrument_base import Instrument
 import SerialPortCommunication as SPC
+import numpy as np
 
 class IVVI_BiasDAC(Instrument):
     """
@@ -57,7 +59,7 @@ class IVVI_BiasDAC(Instrument):
 
     class IVVI_DAC_Group:
         """
-        Helper class for keeping track of set DAC ranges at IVVI D5 module and binary <-> mV conversion
+        Helper class for keeping track of set DAC ranges at IVVI D5 module and binary <-> V conversion
         IVVI can switch DAC polization between negative, centered around 0 and positive in groups of 4. 
         """
         def __init__(self, val1: float, val2: float):
@@ -65,50 +67,74 @@ class IVVI_BiasDAC(Instrument):
             self.high = max(val1, val2)
         def __str__(self) -> str:
             return "IVVI DAC range helper object for {} to {} mV".format(self.low, self.high)
-        def mv2bytes(self, mvolt: float) -> tuple[int, int]:
-            if mvolt < self.low or mvolt > self.high:
+        def v2bytes(self, volt: float) -> tuple[int, int]:
+            if volt < self.low or volt > self.high:
                 raise ValueError
             else:
-                bytevalue = int(round(65535*(mvolt - self.low)/(self.high - self.low)))
+                bytevalue = int(round(65535*(volt - self.low)/(self.high - self.low)))
                 return (bytevalue//256, bytevalue % 256)
-        def bytes2mv(self, byte_high: int, byte_low: int) -> float:
+        def bytes2v(self, byte_high: int, byte_low: int) -> float:
             # Correct readback expected, no value checking
             return self.low + (self.high - self.low)*(256*byte_high + byte_low)/65536
 
     # available ranges
-    DACRangeNeg = IVVI_DAC_Group(-4000, 0)
-    DACRangeBip = IVVI_DAC_Group(-2000, 2000)
-    DACRangePos = IVVI_DAC_Group(0, 4000)
-    RangeIdent = { "pos": DACRangePos, "bip": DACRangeBip, "neg": DACRangeNeg, 
-                "Pos": DACRangePos, "Bip": DACRangeBip, "Neg": DACRangeNeg, 
-                "POS": DACRangePos, "BIP": DACRangeBip, "NEG": DACRangeNeg, 
-                "+"  : DACRangePos, "0"  : DACRangeBip, "-"  : DACRangeNeg  }
+    DACRangeNeg = IVVI_DAC_Group(-4.0, 0.0)
+    DACRangeBip = IVVI_DAC_Group(-2.0, 2.0)
+    DACRangePos = IVVI_DAC_Group(0.0, 4.0)
+    RangeIdent = {  "pos": DACRangePos, "bip": DACRangeBip, "neg": DACRangeNeg, 
+                    "Pos": DACRangePos, "Bip": DACRangeBip, "Neg": DACRangeNeg, 
+                    "POS": DACRangePos, "BIP": DACRangeBip, "NEG": DACRangeNeg, 
+                    "+"  : DACRangePos, "0"  : DACRangeBip, "-"  : DACRangeNeg  }
+    
+    class DummyIVD:
+        """
+        This is a primitive dummy for using the IVVI as a IVD. 
+        It only takes the value-setting side into consideration (voltage divider v_div or current source dAdV), 
+        while e.g. amplification factors when measuring values should be handled within the measure-function.
+        Setting status on/off of additionally needed devices should also be handled outside of this class
+        """
+        def __init__(self):
+            """
+            These parameters need to be set before sweeping
+            """
+            self.measure_func = None
+            self.sweep_channel = None
+            self.delay_setget = None
+            self.pseudo_bias_mode = None # 0: current bias; 1: voltage bias
+            self.dAdV = None # for current bias
+            self.v_div = None # for voltage bias
+        def get_sweep_mode(self):
+            # 0: V bias V measure, 1: I bias V measure, 2: V bias I measure
+            # technically mode 2 also possible, this class cant determine 0 vs 2 without hidden knowledge about measure function though
+            return 1 - self.pseudo_bias_mode
+        def get_sweep_bias(self):
+            return self.pseudo_bias_mode
+        def get_sweep_channels(self):
+            return [self.sweep_channel]
+        def set_status(self, *args, **kwargs):
+            pass
 
-    def __init__(self, name: str, connection: SPC.SerialPortCommunication, knob_config: list[str] = ["0", "0", "0", "0"], reset_on_init: bool = True):
+    def __init__(self, name: str, connection: SPC.SerialPortCommunication, knob_config: list[str] = ["0", "0", "0", "0"], reset_on_init: bool = True, make_dummy_ivd: bool = False):
         """
         Initialzes the IVVI Bias DAC by communicating via serial port
 
         Input:
-            name (str)            : name of the instrument
-            connection_type (str) : how to connect to the 
-            knob_config (list)    : initially set DAC group ranges
-            reset_on_init (bool)  : reset all dacs to 0 after initialization or keep as is
-            **kwargs (if SerialPort)   
-                port (str)        : name of the to be connected to port
-                timeout (float)   : timeout passed to underlying pyserial object (write & read)
-                timeskip (float)  : buffer time for waiting for underlying pyserial object to update sent/received data
-            **kwargs (if ethernet)
-                address (string)  : IP-Adress of to be connected to device
+            name (str)                              : name of the instrument
+            connection (SerialPortCommunication)    : instrument handling communication, see module
+            knob_config (list[str])                 : initially set DAC group ranges
+            reset_on_init (bool)                    : reset all dacs to 0 after initialization or keep as is
+            make_dummy_ivd (bool)                   : include parameters for using IVVI as IVD
 
         Output:
             None
         """
+        self.dummy_ivd = self.DummyIVD() if make_dummy_ivd else None
         Instrument.__init__(self, name, tags=['physical'])
         self.connection = connection
         for j in range(16):
             self.__dict__["do_set_DAC_{}_voltage".format(j + 1)] = lambda mVolt, j=j: self.set_dac(j + 1, mVolt)
             self.__dict__["do_get_DAC_{}_voltage".format(j + 1)] = lambda j=j: self.get_dac(j + 1)
-            self.add_parameter("DAC_{}_voltage".format(j + 1), type=float, minval=self.DACRangeBip.low, maxval=self.DACRangeBip.high, units='mV')
+            self.add_parameter("DAC_{}_voltage".format(j + 1), type=float, minval=self.DACRangeBip.low, maxval=self.DACRangeBip.high, units='V')
         self.dac_ranges = [self.DACRangeBip] * 4
         for i in range(4):
             self.set_dacgroup_range(i, knob_config[i])
@@ -131,6 +157,16 @@ class IVVI_BiasDAC(Instrument):
         """
         del self.connection
         logging.info(__name__ + ': Deleting IVVI instrument')
+    
+    def __getattr__(self, name):
+        """
+        Add DummyIVD attributes to class, if enabled
+        """
+        if not self.dummy_ivd is None:
+            return self.dummy_ivd.__getattribute__(name)
+        else:
+            print("Cannot find attribute '{}' for IVVI_BiasCAC, perhaps dummy IVD needs to be enabled".format(name))
+            raise AttributeError
     
     @staticmethod
     def handle_readback_error_flag(readback: list[int]) -> list[int]:
@@ -182,7 +218,7 @@ class IVVI_BiasDAC(Instrument):
     
     def reset_dacs(self):
         """
-        Set all DACs to 0 mV, adjusted to their currently set range
+        Set all DACs to 0 V, adjusted to their currently set range
 
         Input:
             None
@@ -193,21 +229,21 @@ class IVVI_BiasDAC(Instrument):
         for channel in range(16):
             self.set_dac(channel + 1, 0)
     
-    def set_dac(self, channel: int, mVolt: float):
+    def set_dac(self, channel: int, volt: float):
         """
         Set DAC channel to given voltage, if value is valid within set range
 
         Input:
             channel (int) : 1-based channel index (DAC #1 -> channel = 1, not 0)
-            mVolt (float) : to be set voltage in mV
+            volt (float) : to be set voltage in V
         
         Output:
             None
         """
         try:
-            self.handle_readback_error_flag(self.connection.communicate([7, 0, 2, 1, channel] + list(self.dac_ranges[(channel - 1)//4].mv2bytes(mVolt)), 2))
+            self.handle_readback_error_flag(self.connection.communicate([7, 0, 2, 1, channel] + list(self.dac_ranges[(channel - 1)//4].v2bytes(volt)), 2))
         except ValueError:
-            print("Given voltage {} mV not within selected DAC-range {} .. {} mV".format(mVolt, self.dac_ranges[(channel - 1)//4].low, self.dac_ranges[(channel - 1)//4].high))
+            print("Given voltage {} V not within selected DAC-range {} .. {} V".format(volt, self.dac_ranges[(channel - 1)//4].low, self.dac_ranges[(channel - 1)//4].high))
 
     def get_dacs_raw(self) -> list[int]:
         """
@@ -223,16 +259,16 @@ class IVVI_BiasDAC(Instrument):
     
     def get_dacs_list(self) -> list[float]:
         """
-        Returns list of set DAC voltages in mV
+        Returns list of set DAC voltages in V
 
         Input:
             None
 
         Output:
-            (list[float]) : set DAC voltages in mV
+            (list[float]) : set DAC voltages in V
         """
         data = self.get_dacs_raw()
-        return [self.dac_ranges[channel//4].bytes2mv(data[2*channel], data[2*channel + 1]) for channel in range(16)]
+        return [self.dac_ranges[channel//4].bytes2v(data[2*channel], data[2*channel + 1]) for channel in range(16)]
 
     def get_dac(self, channel: int) -> float:
         """
@@ -242,10 +278,10 @@ class IVVI_BiasDAC(Instrument):
             channel (int) : 1-based channel index (DAC #1 -> channel = 1, not 0)
 
         Output:
-            (float) : voltage in mV
+            (float) : voltage in V
         """
         data = self.get_dacs_raw()
-        return self.dac_ranges[(channel - 1)//4].bytes2mv(data[2*channel - 2], data[2*channel - 1]) 
+        return self.dac_ranges[(channel - 1)//4].bytes2v(data[2*channel - 2], data[2*channel - 1]) 
 
     def get_driver_version(self) -> int:
         """
@@ -263,3 +299,44 @@ class IVVI_BiasDAC(Instrument):
         drive = self.handle_readback_error_flag(self.connection.communicate([4, 0, 3, 4], 3))[0]
         self.set_dac(1, dac_val)
         return drive
+    
+    def get_parameters(self) -> dict:
+        """
+        Returns info for qkit
+        """
+        # DAC values in V
+        params = {"bias_dac_{}".format(i+1): val for i, val in enumerate(self.get_dacs_list())}
+        if self.dummy_ivd is None:
+            ivd_params = {}
+        else:
+            ivd_params = {
+                "measure_func": self.dummy_ivd.measure_func.__name__,
+                "sweep_channel": self.dummy_ivd.sweep_channel,
+                "delay_setget": self.dummy_ivd.delay_setget,
+                "pseudo_bias_mode": self.dummy_ivd.pseudo_bias_mode, 
+                "dAdV": self.dummy_ivd.dAdV,
+                "v_div": self.dummy_ivd.v_div
+            }
+        return {**params, **ivd_params}
+
+    def take_IV(self, sweep):
+        if self.dummy_ivd is None:
+            print("IVVI_BiasDAC needs dummy IVD enabled to take IV-curve")
+            raise AttributeError
+        start, stop, step, _ = sweep
+        setty = []
+        getty = []
+        for val in np.linspace(start, stop, int(round(np.abs(start-stop)/step+1))):
+            if self.dummy_ivd.pseudo_bias_mode: # voltage bias
+                self.set_dac(self.dummy_ivd.sweep_channel, val*self.dummy_ivd.v_div)
+                setty += [self.get_dac(self.dummy_ivd.sweep_channel)/self.dummy_ivd.v_div]
+            else: # current bias
+                self.set_dac(self.dummy_ivd.sweep_channel, val/self.dummy_ivd.dAdV)
+                setty += [self.get_dac(self.dummy_ivd.sweep_channel)*self.dummy_ivd.dAdV]
+            time.sleep(self.dummy_ivd.delay_setget)
+            getty += [self.dummy_ivd.measure_func()]
+        # return requires I-values first, V-values second independent of what was set/measured
+        if self.dummy_ivd.pseudo_bias_mode: # voltage bias
+            return np.array(getty), np.array(setty)
+        else: # current bias
+            return np.array(setty), np.array(getty)
