133f2f0b876d80664393e6ee551b4500c16bdf25
Thilo Krumrey <thilo.krumrey@kit.edu>
2025-01-08 15:11:31 +0100

    Fix missing directory structure

--- tests/test_qkit_basics.py
+++ tests/test_qkit_basics.py
@@ -1,3 +1,14 @@
+import pytest
+
+@pytest.fixture(scope="session", autouse=True)
+def do_something(request):
+    import os
+    import pathlib
+    from qkit.install.install import create_base_structure
+    cwd = pathlib.Path(os.getcwd())
+    print("CWD", cwd)
+    create_base_structure(cwd)
+
 def test_qkit_load():
     import qkit
     qkit.cfg['run_id'] = "Test"
