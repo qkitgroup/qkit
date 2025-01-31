271a22aec185a1e2871c9befb7fc17fa1f8511d1
Thilo Krumrey <thilo.krumrey@kit.edu>
2025-01-31 11:07:23 +0100

    Improved tests.

--- tests/test_qkit_basics.py
+++ tests/test_qkit_basics.py
@@ -1,7 +1,9 @@
 import pytest
 
+import qkit.core
+
 @pytest.fixture(scope="session", autouse=True)
-def do_something(request):
+def setup(request):
     import os
     import pathlib
     from qkit.install.install import create_base_structure
@@ -11,6 +13,14 @@ def do_something(request):
 
 def test_qkit_load():
     import qkit
-    qkit.cfg['run_id'] = "Test"
+    qkit.cfg['fid_scan_datadir'] = False
+    qkit.cfg['run_id'] = "Basic Test"
+    qkit.cfg['user'] = "Automated Test"
+    qkit.start()
+
+def test_load_datadir():
+    import qkit
+    qkit.cfg['fid_scan_datadir'] = True
+    qkit.cfg['run_id'] = "Basic Test"
     qkit.cfg['user'] = "Automated Test"
     qkit.start()
