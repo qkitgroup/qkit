f355c10804070dae09496ef275a8c6dc91595dfe
Marius-Frohn <uzrfo@student.kit.edu>
2025-10-21 17:47:58 +0200

    AAAA

--- src/qkit/analysis/crit_detection_iv.py
+++ src/qkit/analysis/crit_detection_iv.py
@@ -2,6 +2,7 @@ import itertools
 from typing import override, Any, Literal
 from enum import Enum
 from scipy import signal
+import numpy as np
 
 from qkit.measure.unified_measurements import AnalysisTypeAdapter, MeasurementTypeAdapter, DataView, DataViewSet, DataReference
 from qkit.analysis.numerical_derivative import SavgolNumericalDerivative
@@ -30,38 +31,61 @@ class CritDetectionIV(AnalysisTypeAdapter):
 
     @override
     def perform_analysis(self, data: tuple['MeasurementTypeAdapter.GeneratedData', ...]) -> tuple['MeasurementTypeAdapter.GeneratedData', ...]:
-        data[0].descriptor.axes[:-1]
-        data[0].descriptor.name
-        return ()
+        parent_schema = tuple([element.descriptor for element in data])
+        output_schema = self.expected_structure(parent_schema)
+        out = []
+        flipBiasMeas = (parent_schema[0].name == "i_0") ^ (self._onWhat in ["v", "dvdi"])
+        if self._onWhat in ["di_dv", "dv_di"]:
+            data = self._numderHelper.perform_analysis(data)
+        for ((dxdy, dydx), (x, y)) in zip(itertools.batched(output_schema, 2), itertools.batched(data, 2)):
+            out.append(dxdy.with_data(signal.savgol_filter(x.data, **self.savgol_kwargs)/signal.savgol_filter(y.data, **self.savgol_kwargs)))
+            out.append(dydx.with_data(signal.savgol_filter(y.data, **self.savgol_kwargs)/signal.savgol_filter(x.data, **self.savgol_kwargs)))
+        return tuple(out)
+
+    def _crit_find_thresh(x_vals: np.ndarray, y_vals: np.ndarray, thresh: float = 1e-6) -> tuple[np.ndarray]:
+        """
+        helper function for main threshold detection on y-vals, x-vals needed for sanity checking
+        data should be flipped & mirrored to ideally look like 
+        ^ x_vals (.), y_vals (x), crits (o)
+        |                         .x
+        |                      . x 
+        |                   .   x
+        |                .      
+      0 +------oxxxxxxxxxxxxxxxo-----> #idx
+        |     x    .
+        |    x  .    
+        |   x.
+        | .x
+        v
+        """
+        # thresh detect
+        upper_idxs = np.argmax(np.logical_and(y_vals > thresh, x_vals > 0), axis=-1)
+        upper_idxs = np.where(np.any(np.logical_and(y_vals > thresh, x_vals > 0), axis=-1), upper_idxs, x_vals.shape[-1] - 1) # default to max if no tresh found
+        lower_idxs = y_vals.shape[-1] - 1 - np.argmax(np.flip(np.logical_and(y_vals > thresh, x_vals < 0), axis=-1), axis=-1) # flip because argmax returns first occurence
+        lower_idxs = np.where(np.any(np.logical_and(y_vals > thresh, x_vals < 0), axis=-1), lower_idxs, 0)
+        return upper_idxs, lower_idxs
+
 
     @override
     def expected_structure(self, parent_schema: tuple['MeasurementTypeAdapter.DataDescriptor', ...]) -> tuple['MeasurementTypeAdapter.DataDescriptor', ...]:
-        return ()
+        structure = []
+        for i, bias in enumerate(parent_schema[::2]):
+            structure += [
+                MeasurementTypeAdapter.DataDescriptor(
+                    name=f"ic_upper_{i}" if self._onWhat in ["v", "dv_di"] else f"vc_upper_{i}",
+                    unit=f"{bias.unit}",
+                    axes=bias.axes[:-1], 
+                    category="analysis"
+                ),
+                MeasurementTypeAdapter.DataDescriptor(
+                    name=f"ic_lower_{i}" if self._onWhat in ["v", "dv_di"] else f"vc_lower_{i}",
+                    unit=f"{bias.unit}",
+                    axes=bias.axes[:-1], 
+                    category="analysis"
+                ),
+            ]
+        return tuple(structure)
 
     @override
     def default_views(self, parent_schema: tuple['MeasurementTypeAdapter.DataDescriptor', ...]) -> dict[str, DataView]:
         return ()
-    
-    """
-    @override
-    def expected_structure(self, parent_schema: tuple['MeasurementTypeAdapter.DataDescriptor', ...]) -> tuple['MeasurementTypeAdapter.DataDescriptor', ...]:
-        structure = []
-        for (x, y) in itertools.batched(parent_schema, 2):
-            assert x.axes == y.axes
-            structure += [
-                MeasurementTypeAdapter.DataDescriptor(
-                    name=f"d{x.name}_d{y.name}",
-                    unit=f"{x.unit}/{y.unit}",
-                    axes=x.axes, 
-                    category="analysis"
-                ),
-                MeasurementTypeAdapter.DataDescriptor(
-                    name=f"d{y.name}_d{x.name}",
-                    unit=f"{y.unit}/{x.unit}",
-                    axes=x.axes,
-                    category="analysis"
-                )
-            ]
-        return tuple(structure)
-    """
-
