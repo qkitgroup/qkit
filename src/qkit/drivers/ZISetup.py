95b6cb2f9d3df9f1242a4e0ba93260ca651e7d3f
Thilo Krumrey <thilo.krumrey@kit.edu>
2026-06-11 10:12:12 +0200

    Fix off by one error.

--- src/qkit/drivers/ZISetup.py
+++ src/qkit/drivers/ZISetup.py
@@ -23,7 +23,7 @@ class ZISetup(Instrument):
         parameters = self._qubits[0].parameters
         attributes = [entry for entry in dir(parameters) if not entry.startswith('_') and not callable(getattr(parameters, entry))]
         for key in attributes:
-            self.add_parameter(key, channels=(0, len(self._qubits)),
+            self.add_parameter(key, channels=(0, len(self._qubits)-1),
                                get_cmd=lambda channel: getattr(self._qubits[channel].parameters, key),
                                set_cmd=lambda value, channel: setattr(self._qubits[channel].parameters, key, value))
 
